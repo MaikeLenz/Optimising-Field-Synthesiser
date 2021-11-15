4694698c84d1daacb205bba0148c8034cfb2f57b
MaikeLenz <maikelenz5@gmail.com>
2021-11-15 11:56:25 +0000

    anaysis of chipped and line window

---
+++ image analysis/chip2_background_subtraction.py
@@ -0,0 +1,54 @@
+import PIL as PIL
+import matplotlib.pyplot as plt
+import numpy as np
+#%%
+# Analysis of quantitative measurements
+# Open images and check sizes
+
+#undamaged side of chip window
+
+im1 = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\chip_window\\Chip2_0.PNG')
+im2 = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\chip_window\\Chip2_1.PNG')
+im3 = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\chip_window\\Chip2_2.PNG')
+im4 = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\chip_window\\Chip2_3.PNG')
+im5 = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\chip_window\\Chip2_4.PNG')
+im6 = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\chip_window\\Chip2_5.PNG')
+im7 = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\chip_window\\Chip2_6.PNG')
+im8 = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\chip_window\\Chip2_7.PNG')
+im9 = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\chip_window\\Chip2_8.PNG')
+im10 = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\chip_window\\Chip2_9.PNG')
+im11= PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\chip_window\\Chip2_10.PNG')
+im12= PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\chip_window\\Chip2_11.PNG')
+im13 = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\chip_window\\Chip2_12.PNG')
+
+imB = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\chip_window\\Chip2_Background.PNG')
+images = [im1,im2,im3, im4, im5, im6, im7, im8, im9, im10,im11,im12,im13]
+
+"""
+for image in images:
+    newimage = PIL.ImageChops.subtract(image, im5)
+
+    mask1 = PIL.Image.eval(newimage, lambda a: 0) #if a <= 24 else 255)
+    mask2 = mask1.convert('1')
+
+    blank = PIL.Image.eval(newimage, lambda a: 0)
+
+    new = PIL.Image.composite(newimage, blank, mask2) 
+
+    #diff= PIL.ImageChops.difference(image, imB)
+
+    plt.figure()
+    plt.imshow(np.asarray(new), interpolation='nearest', cmap="gray")
+    #plt.imshow(np.asarray(diff), interpolation='nearest', cmap="gray")
+
+    plt.colorbar()
+plt.show()
+"""
+
+for image in images:
+    newimage2_array=np.asarray(image.convert('L'))-np.asarray(imB.convert('L'))
+    #newimage2=PIL.Image.fromarray(newimage2)
+    plt.figure()
+    plt.imshow(newimage2_array, interpolation='nearest')
+    plt.colorbar()
+plt.show()
---
+++ image analysis/line_background_subtraction.py
@@ -0,0 +1,55 @@
+import PIL as PIL
+import matplotlib.pyplot as plt
+import numpy as np
+#%%
+# Analysis of quantitative measurements
+# Open images and check sizes
+
+
+
+im1 = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\line_window\\Line_0.PNG')
+im2 = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\line_window\\Line_1.PNG')
+im3 = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\line_window\\Line_2.PNG')
+im4 = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\line_window\\Line_3.PNG')
+im5 = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\line_window\\Line_4.PNG')
+im6 = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\line_window\\Line_5.PNG')
+im7 = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\line_window\\Line_6.PNG')
+im8 = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\line_window\\Line_7.PNG')
+im9 = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\line_window\\Line_8.PNG')
+im10 = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\line_window\\Line_9.PNG')
+im11= PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\line_window\\Line_10.PNG')
+im12= PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\line_window\\Line_11.PNG')
+im13 = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\line_window\\Line_12.PNG')
+im14 = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\line_window\\Line_13.PNG')
+
+imB = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\line_window\\LineG_Background.PNG')
+images = [im1,im2,im3, im4, im5, im6, im7, im8, im9, im10,im11,im12,im13,im14]
+
+"""
+for image in images:
+    newimage = PIL.ImageChops.subtract(image, im5)
+
+    mask1 = PIL.Image.eval(newimage, lambda a: 0) #if a <= 24 else 255)
+    mask2 = mask1.convert('1')
+
+    blank = PIL.Image.eval(newimage, lambda a: 0)
+
+    new = PIL.Image.composite(newimage, blank, mask2) 
+
+    #diff= PIL.ImageChops.difference(image, imB)
+
+    plt.figure()
+    plt.imshow(np.asarray(new), interpolation='nearest', cmap="gray")
+    #plt.imshow(np.asarray(diff), interpolation='nearest', cmap="gray")
+
+    plt.colorbar()
+plt.show()
+"""
+
+for image in images:
+    newimage2_array=np.asarray(image.convert('L'))-np.asarray(imB.convert('L'))
+    #newimage2=PIL.Image.fromarray(newimage2)
+    plt.figure()
+    plt.imshow(newimage2_array, interpolation='nearest')
+    plt.colorbar()
+plt.show()
