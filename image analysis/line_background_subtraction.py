48883419b10b467e2ecb545e5d739123217bb2cd
Molly Smith <93262796+iammollys@users.noreply.github.com>
2021-11-22 11:10:41 +0000

    Update line_background_subtraction.py

--- image analysis/line_background_subtraction.py
+++ image analysis/line_background_subtraction.py
@@ -45,6 +45,7 @@ for image in images:
     plt.colorbar()
 plt.show()
 """
+"""
 f = plt.figure(constrained_layout=True)
 gs = f.add_gridspec(3,4)
 
@@ -63,13 +64,22 @@ for i in range(len(images)):
         f_ax = f.add_subplot(gs[2, i-8])
         f_ax.imshow(newimage2_array, cmap='gray')
 plt.show()
+"""
+#plt.figure()
+#y_shift = 1/(0.006*13.4615)
+y_shift = 0
+pixel_size = 0.006
+beam_size_large = 10.5
+beam_size_small = 0.78
+magnification = beam_size_large/beam_size_small
+scaling = pixel_size*magnification
 
-#%%
-y_shift = 1/(0.006*13.4615)
+crop_size = [200, 300, 300, 450]
+
 for i in range(len(images)):
     newimage2_array=np.asarray(images[i].convert('L'))-np.asarray(imB.convert('L'))
 
-    newimage2_array=newimage2_array[200:300,300:450]
+    newimage2_array=newimage2_array[crop_size[0]:crop_size[1],crop_size[2]:crop_size[3]]
     
     # Shift by 1mm*i
     amount_taken_off = len(newimage2_array) - int(y_shift*i)
@@ -77,7 +87,8 @@ for i in range(len(images)):
         pass
     else:
         length = len(newimage2_array[0])
-        newimage2_array = newimage2_array[int(y_shift*i):]
-        newimage2_array = np.append(newimage2_array, np.zeros((amount_taken_off, length)), axis=0)
-        print(newimage2_array)
-        plt.imshow(newimage2_array, cmap='gray', alpha=0.2)
+        #newimage2_array = newimage2_array[int(y_shift*i):]
+        #newimage2_array = np.append(newimage2_array, np.zeros((amount_taken_off, length)), axis=0)
+        #print(newimage2_array)
+        plt.imshow(newimage2_array, cmap='inferno', alpha=0.15, extent=[crop_size[2]*scaling,crop_size[3]*scaling,crop_size[0]*scaling,crop_size[1]*scaling])
+
