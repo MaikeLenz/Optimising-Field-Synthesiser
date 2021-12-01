0dc394013c76323d12dc9c002137ba95df012434
MaikeLenz <maikelenz5@gmail.com>
2021-12-01 10:49:50 +0000

    introduced window which cannot be cropped out

--- BO/ErrorCorrectionFunction_integrate.py
+++ BO/ErrorCorrectionFunction_integrate.py
@@ -1,74 +1,57 @@
 
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from scipy import integrate
 
 
-def errorCorrection_int(t, E1, E2):
+def errorCorrection_int(t, synth_field, goal_field):
     """
-    returns the negative of the rms error between E1 and E2
+    returns the negative of the rms error between synth_field and goal_field
     without any adjustment in amplitudes or position in time
     """
     diff = np.array([])
-    if len(E1) != len(E2):
-        print('Error- E1 is a different length to E2')
-    for i in range(len(E1)):
-        diff=np.append(diff,[(E1[i] - E2[i])**2])
-    return -integrate.simps(diff, t)
+    if len(synth_field) != len(goal_field):
+        print('Error- synth_field is a different length to goal_field')
+    for i in range(len(synth_field)):
+        diff=np.append(diff,[(synth_field[i] - goal_field[i])**2])
+    return - integrate.simps(diff, t)
 
-
-def errorCorrectionAdvanced_int(t, E1, E2):
+def errorCorrectionAdvanced_int(t, synth_field, goal_field):
     """
     finds rms error between two functions, first scales them and matches up maxima in time.
     """
-    if len(E1) != len(E2):
-        print('Error- E1 is a different length to E2')
-    """  
-    plt.figure()
-    plt.plot(t, E1, label='curve 1')
-    plt.plot(t, E2, label='curve 2')
-    plt.title('Raw data')
-    plt.legend()
-    """
+    if len(synth_field) != len(goal_field):
+        print('Error- synth_field is a different length to goal_field')
      
     # First scale both functions to amplitude of 1
-    if np.linalg.norm(np.array(E1)) != 0:
-        E1=np.array(E1)/(np.linalg.norm(np.array(E1))) #normalise arrays
-    if np.linalg.norm(np.array(E2)) != 0:
-        E2=np.array(E2)/(np.linalg.norm(np.array(E2)))
-    """
-    plt.figure()
-    plt.plot(t, E1, label='curve 1, scaled')
-    plt.plot(t, E2, label='curve 2, scaled')
-    plt.title('Amplitude Scaled')
-    plt.legend()
-    """   
+    if np.linalg.norm(np.array(synth_field)) != 0:
+        synth_field=np.array(synth_field)/(np.linalg.norm(np.array(synth_field))) #normalise arrays
+    if np.linalg.norm(np.array(goal_field)) != 0:
+        goal_field=np.array(goal_field)/(np.linalg.norm(np.array(goal_field)))
+
     # Next shift the maximums to the same time values- consider only the first maximum for simplicity
 
-    offset=np.argmax(E1)-np.argmax(E2)
+    #offset=np.argmax(synth_field)-np.argmax(goal_field)
+    max_indices_synth = np.argwhere(synth_field == np.amax(synth_field)).flatten().tolist()
+    max_indices_goal = np.argwhere(goal_field == np.amax(goal_field)).flatten().tolist()
+    median_synth = median(max_indices_synth)
+    median_goal = median(max_indices_goal) 
+    offset = median_synth - median_goal
 
-    if offset > 0: #E2 on the left of E1
-        E2 = E2[:len(E1)-abs(offset)]
-        E2=np.append(np.zeros(abs(offset)),E2)
-    elif offset < 0: #E2 on the right of E1
-        E2 = E2[abs(offset):]
-        E2=np.append(E2,np.zeros(abs(offset)))
+    #determine point that falls below 10% of max which is the limit for being cut off
+    args_below_ten = np.argwhere(synth_field <= 0.1*np.amax(synth_field))#
+    args_below_ten_reverse = args_below_ten[::-1] #reverses array
+    edge_left = np.argwhere(args_below_ten_reverse < median_synth)
+    edge_right = np.argwhere(args_below_ten > median_synth)
 
-    """
-    E2_shifted = np.zeros(len(E2))
-    for i in range(len(E2)):
-        if (i+E1_max_index[0]-E2_max_index[0]) < len(E2):
-            E2_shifted[i] = E2[i+E1_max_index[0]-E2_max_index[0]]
-    """
-    """
-    plt.figure()
-    plt.plot(t, E1, label='curve 1')
-    plt.plot(t, E2, label ='curve 2, shifted in time')
-    plt.title('E2 shifted in time')
-    plt.legend()
-    plt.show()
-    """
-    return errorCorrection_int(t,E1,E2)
+    if offset > 0 and (len(synth_field)-abs(offset))>edge_right: #goal_field on the left of synth_field
+        goal_field = goal_field[:len(synth_field)-abs(offset)]
+        goal_field=np.append(np.zeros(abs(offset)),goal_field)
+    elif offset < 0 and (abs(offset)<edge_left): #goal_field on the right of synth_field
+        goal_field = goal_field[abs(offset):]
+        goal_field=np.append(goal_field,np.zeros(abs(offset)))
+
+    return errorCorrection_int(t,synth_field,goal_field)
 
 
 def Gauss(x, A, u, o):
@@ -79,48 +62,48 @@ def Gauss(x, A, u, o):
 #%%
 # Testing errorCorrection
 t = np.arange(0, 50, 0.1)
-E1 = Gauss(t, 1, 25, 5)
+synth_field = Gauss(t, 1, 25, 5)
 
 
 # Test function against itself
 plt.figure()
-plt.plot(t, E1, label='original')
+plt.plot(t, synth_field, label='original')
 plt.legend()
-print(errorCorrection(t, E1, E1)) # Should be zero
+print(errorCorrection(t, synth_field, synth_field)) # Should be zero
 
 
 # Test against function shifted in time (delayed)
-E2 = Gauss(t+5, 1, 25, 5)
+goal_field = Gauss(t+5, 1, 25, 5)
 plt.figure()
-plt.plot(t, E1, label='original')
-plt.plot(t, E2, label='shifted')
+plt.plot(t, synth_field, label='original')
+plt.plot(t, goal_field, label='shifted')
 plt.legend()
-print(errorCorrection(t, E1, E2))
+print(errorCorrection(t, synth_field, goal_field))
 
 # Shift more in time- should be a larger error
-E2 = Gauss(t+10, 1, 25, 5)
+goal_field = Gauss(t+10, 1, 25, 5)
 plt.figure()
-plt.plot(t, E1, label='original')
-plt.plot(t, E2, label='shifted')
+plt.plot(t, synth_field, label='original')
+plt.plot(t, goal_field, label='shifted')
 plt.legend()
-print(errorCorrection(t, E1, E2))
+print(errorCorrection(t, synth_field, goal_field))
 
 # Change amplitude and width
-E2 = Gauss(t, 5, 25, 5)
+goal_field = Gauss(t, 5, 25, 5)
 plt.figure()
-plt.plot(t, E1, label='original')
-plt.plot(t, E2, label='shifted')
-print(errorCorrection(t, E1, E2))
+plt.plot(t, synth_field, label='original')
+plt.plot(t, goal_field, label='shifted')
+print(errorCorrection(t, synth_field, goal_field))
 E3 = Gauss(t, 1, 25, 10)
 plt.plot(t, E3, label='broader')
 plt.legend()
-print(errorCorrection(t, E1, E3))
+print(errorCorrection(t, synth_field, E3))
 """
 #%%
 """
 # Test advanced error correction function
 t = np.arange(0, 50, 0.1)
-E1 = Gauss(t, 1, 25, 5)
-E2 = Gauss(t+5, 5, 25, 5)
-errorCorrectionAdvanced(t, E1, E2)
+synth_field = Gauss(t, 1, 25, 5)
+goal_field = Gauss(t+5, 5, 25, 5)
+errorCorrectionAdvanced(t, synth_field, goal_field)
 """
