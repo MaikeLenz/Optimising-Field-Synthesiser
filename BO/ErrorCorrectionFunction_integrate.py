7c4f0d9a9f4c56c0cd4a2111bec71a6770939a55
MaikeLenz <maikelenz5@gmail.com>
2022-01-11 14:01:03 +0000

    updated comments

--- BO/ErrorCorrectionFunction_integrate.py
+++ BO/ErrorCorrectionFunction_integrate.py
@@ -1,20 +1,21 @@
 
 import numpy as np
-#import matplotlib.pyplot as plt
 from scipy import integrate
-
+#defines the rms error minimisation functions. 
+#errorCorrection_int just works with two given arrays.
+#errorCorrectionAdvanced_int normalises them and shifts them in time to align before calculating the errors.
 
 def errorCorrection_int(t, synth_field, goal_field):
     """
     returns the negative of the rms error between synth_field and goal_field
     without any adjustment in amplitudes or position in time
     """
-    diff = np.array([])
+    diff = np.array([]) #array of the square of differences between intensity and goal function
     if len(synth_field) != len(goal_field):
         print('Error- synth_field is a different length to goal_field')
     for i in range(len(synth_field)):
         diff=np.append(diff,[(synth_field[i]**2 - goal_field[i])**2])
-    return - integrate.simps(diff, t)
+    return - integrate.simps(diff, t) #integrates the differences
 
 def errorCorrectionAdvanced_int(t, synth_field, goal_field):
     """
@@ -24,14 +25,14 @@ def errorCorrectionAdvanced_int(t, synth_field, goal_field):
         print('Error- synth_field is a different length to goal_field')
      
     # First scale both functions to amplitude of 1
+    #need to check that the norm isn't zero to avoid errors
     if np.linalg.norm(np.array(synth_field)) != 0:
         synth_field=np.array(synth_field)/(np.linalg.norm(np.array(synth_field))) #normalise arrays
     if np.linalg.norm(np.array(goal_field)) != 0:
         goal_field=np.array(goal_field)/(np.linalg.norm(np.array(goal_field)))
 
-    # Next shift the maximums to the same time values- consider only the first maximum for simplicity
+    # Next shift the maxima to the same time values- consider only the first maximum for simplicity
 
-    #offset=np.argmax(synth_field)-np.argmax(goal_field)
     max_indices_synth = np.argwhere(synth_field == np.amax(synth_field)).flatten().tolist()
     max_indices_goal = np.argwhere(goal_field == np.amax(goal_field)).flatten().tolist()
     median_synth = median(max_indices_synth)
@@ -53,12 +54,13 @@ def errorCorrectionAdvanced_int(t, synth_field, goal_field):
         goal_field = goal_field[abs(offset):]
         goal_field=np.append(goal_field,np.zeros(abs(offset)))
 
-    return errorCorrection_int(t,synth_field,goal_field)
+    return errorCorrection_int(t,synth_field,goal_field) #no carry out rms minimisation with aligned and normalised arrays
 
 
 def Gauss(x, A, u, o):
     return A*np.exp(-((x-u)**2)/(2*(o**2)))
 
+#testing code 
 
 """
 #%%
