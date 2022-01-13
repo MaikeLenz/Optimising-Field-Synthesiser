0a533b7fc0d4da24d23268bbe561e507807a2cf3
MaikeLenz <maikelenz5@gmail.com>
2022-01-13 23:33:42 +0000

    updated inverse FT

--- image analysis/Mako/inverse_FT.py
+++ image analysis/Mako/inverse_FT.py
@@ -3,8 +3,15 @@ import matplotlib.pyplot as plt
 import PIL as PIL
 
 #take image 9 of the line transmission and inverse FT
-img = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\Mako\\line\\4.PNG')
+
+img = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\Mako\\line\\5.PNG')
 imB = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\Mako\\line\\Background.PNG')
+
+"""
+#high force clamping
+img= PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\Forces\\5_08ohms\\14.PNG')
+imB = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\Forces\\5_08ohms\\Background.PNG')
+"""
 
 imB_array =np.asarray(imB.convert('L'))
 imB_array =imB_array.astype(np.int16)
@@ -13,10 +20,11 @@ im_array= np.asarray(img.convert('L'))
 im_array=im_array.astype(np.int16)
 newimage_array=im_array-imB_array
 slicing=[650,820,900,1100]
-newimage_array=newimage_array[slicing[0]:slicing[1],slicing[2]:slicing[3]]
+#newimage_array=newimage_array[slicing[0]:slicing[1],slicing[2]:slicing[3]]
 centre=[-slicing[0]+slicing[1],-slicing[2]+slicing[3]]
 
-inv= np.fft.ifftshift(np.fft.ifft2(newimage_array))
+inv= np.fft.ifftshift(np.fft.ifft2(newimage_array)) #inverse FT and shift zero freq components to centre
+#inv= np.fft.ifft2(newimage_array)
 inv = np.log(np.abs(inv))
 #inv[inv > 255] = 255
 
