eac3a1f0589575f07a0ca8fb5dbc82750a0a6278
MaikeLenz <maikelenz5@gmail.com>
2022-01-17 15:57:09 +0000

    image analysis of repeats

---
+++ image analysis/75mm_lens_setup/split_rep1.py
@@ -0,0 +1,47 @@
+import PIL as PIL
+import matplotlib.pyplot as plt
+import numpy as np
+#%%
+# Analysis of quantitative measurements
+# Open images and check sizes
+
+#damaged side of Split window
+
+im1 = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\75mm_lens_setup\\repeat 1\\Split\\0.PNG')
+im2 = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\75mm_lens_setup\\repeat 1\\Split\\4.PNG')
+imB = PIL.Image.open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\images\\75mm_lens_setup\\repeat 1\\Split\\Background.PNG')
+
+images = [im1,im2]
+
+
+imB_array =np.asarray(imB.convert('L'))
+imB_array =imB_array.astype(np.int16)
+
+f = plt.figure(constrained_layout=True)
+gs = f.add_gridspec(1,2)
+
+for i in range(len(images)):
+    im_array= np.asarray(images[i].convert('L'))
+    im_array=im_array.astype(np.int16)
+    newimage2_array=im_array-imB_array
+
+    #newimage2_array=newimage2_array[600:1000,900:1300]
+    max=3
+    min=-5
+    if i >=0 and i<3:
+        f_ax = f.add_subplot(gs[0,i])
+        im=f_ax.imshow(newimage2_array, vmin=min, vmax=max,cmap='gray')
+        #f.colorbar(im)
+    elif i<6:
+        f_ax = f.add_subplot(gs[1, i-3])
+        im=f_ax.imshow(newimage2_array, vmin=min, vmax=max,cmap='gray')
+        #f.colorbar(im)
+    elif i<9:
+        f_ax = f.add_subplot(gs[2, i-6])
+        im=f_ax.imshow(newimage2_array, vmin=min, vmax=max,cmap='gray')
+        #f.colorbar(im)
+f.subplots_adjust(right=0.8)
+cbar_ax = f.add_axes([0.85, 0.15, 0.015, 0.7])
+plt.suptitle("Split window, 4mm increments")
+f.colorbar(im, cax=cbar_ax)
+plt.show()
