a5cc0762127e74f01a7007126122c21a1e34b6ec
MaikeLenz <maikelenz5@gmail.com>
2022-02-01 13:44:41 +0000

    got Et to work

--- HCF sim/python sim/Sim_python.py
+++ HCF sim/python sim/Sim_python.py
@@ -36,27 +36,32 @@ Main.energy = energy
 # setting pressure to (0,pressure) means a gradient from zero up until given value
 #Main.duv = Main.eval('prop_capillary(radius, flength, gas, pressure; λ0, τfwhm, energy, modes=4, trange=400e-15, λlims=(150e-9, 4e-6))')
 Main.duv = Main.eval('duv = prop_capillary(radius, flength, gas, pressure; λ0, τfwhm, energy, trange=400e-15, λlims=(150e-9, 4e-6))')
-"""
-Main.eval('dumps = duv["dumps"]')
-dumps = Main.dumps
-"""
+
 Main.eval("λ, Iλ = Processing.getIω(duv, :λ, flength)")
-#Main.eval('t, Et, zactual = Processing.getEt(output["Eω"])')
+Main.eval('t, Et = Processing.getEt(duv)')
 
 ## These next lines show how t, Et and zactual could be accessed in the Python namespace for analysis
 # Once defined, the same method as in time_1d can be used to get the y data needed for analysis of the curve (max heigh, FWHM, etc.), which
 # subsequently would be the inputs the the BO 
-
-# These print out t, Et and zactual at the moment, which fills the terminal. The first step is to mute this printing (perhaps a kwarg in eval?)
-
-#Main.eval("t[0:10]")
-
 λ = Main.λ
 Iλ = Main.Iλ
+t=Main.t
 
+Et_allz=Main.Et #array of Et at all z 
+Et=Et_allz[-1]
+Et0=Et_allz[0]
 
-print(Main)
+"""
 print(λ[0:10])
 print(Iλ[0:10])
+print(t[0:10])
+print(Et[0:10])
+"""
+
+plt.figure()
 plt.plot(λ,Iλ)
+plt.figure()
+plt.plot(t,Et,label=("z=1m"))
+plt.plot(t,Et0,label="z=0")
+plt.legend()
 plt.show()
