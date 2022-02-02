4e396f529df32b3edd5989a2dfebf69c8f2c7adb
MaikeLenz <maikelenz5@gmail.com>
2022-02-02 18:28:56 +0000

    plotted bandwidth vs pulse energy

--- HCF sim/Python/building datasets/energy_vs_rms_width.py
+++ HCF sim/Python/building datasets/energy_vs_rms_width.py
@@ -15,7 +15,7 @@ gas = "Ne"
 pressure = 2.340607 # gas pressure in bar, corresponds to 66% of 3.5 atm
 λ0 = 800e-9 # central wavelength of the pump pulse
 τfwhm = 30e-15 # FWHM duration of the pump pulse
-energies = np.linspace(0.1e-3,1e-3,100) # array of energies in the pump pulse
+energies = np.linspace(0.1e-3,1e-3,10) # array of energies in the pump pulse
 
 # Assign arguments to Main namespace
 Main.radius = radius
@@ -37,7 +37,7 @@ for i in energies:
     Main.duv = Main.eval('duv = prop_capillary(radius, flength, gas, pressure; λ0, τfwhm, energy, trange=400e-15, λlims=(150e-9, 4e-6))')
 
     #now extract datasets
-    Main.eval("ω, Iω = Processing.getIω(duv, ω:, flength)")
+    Main.eval("ω, Iω = Processing.getIω(duv, :ω, flength)")
     Main.eval('t, Et = Processing.getEt(duv)')
 
     ## These next lines show how t, Et and zactual could be accessed in the Python namespace for analysis
@@ -46,9 +46,11 @@ for i in energies:
     #assign python variables
     ω = Main.ω
     Iω = Main.Iω
-
     width=rms_width(ω,Iω)
     widths=np.append(widths,width)
 
 plt.scatter(energies,widths)
+plt.grid()
+plt.ylabel("angular frequency width, /s")
+plt.xlabel("Pulse energy, J")
 plt.show()
