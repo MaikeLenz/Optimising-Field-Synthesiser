0bcf1051d464d62a12a33f137f34bc861a655bb1
MaikeLenz <maikelenz5@gmail.com>
2022-02-02 23:06:55 +0000

    fixed dependencies

--- BO/bossfunction.py
+++ BO/bossfunction.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 import sys
 sys.path.append('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\code\\Synth\\Optimising-Field-Synthesiser\\BO\\')
-sys.path.append('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\code\\Synth\\Optimising-Field-Synthesiser\\synth_sim\\')
+sys.path.append('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\code\\Synth\\Optimising-Field-Synthesiser\\BO\\synthesiser_simulation\\')
 from subtargetfunctions import *
 from field_synth_class import *
 #from ErrorCorrectionFunction import *
@@ -182,24 +182,3 @@ def BO(params, Synth, function, init_points=50, n_iter=50, goal_field=None, t=np
             f_ax.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
 
     plt.show()
-
-#testing
-
-"""
-#create fields
-Field1=Wavepacket(t0=0.0, wavel=400.0, fwhm=10.0, amp=1.0, CEP=0.0)
-Field2=Wavepacket(t0=0.0, wavel=700.0, fwhm=20.0, amp=1.0, CEP=0.0)
-Field3=Wavepacket(t0=0.0, wavel=1000.0, fwhm=30.0, amp=1.0, CEP=0.0)
-Field4=Wavepacket(t0=0.0, wavel=2000.0, fwhm=10.0, amp=1.0, CEP=0.0)
-Field5=Wavepacket(t0=0.0, wavel=1500.0, fwhm=15.0, amp=1.0, CEP=0.0)
-
-#initialise pulse list and tuple of relative delays (from 1st pulse)
-pulses=[Field1,Field2, Field3, Field4, Field5]
-delays=(10,20,30,10)
-#pass to synthesiser
-Synth=Synthesiser(pulses,delays)
-
-#parameters to be optimised
-params=['CEP5','CEP2','CEP3','CEP4']
-BO(params, Synth, sharpestPeak_triang, 10,10)
-"""
---
+++ BO/tests/test_bossfunction.py
@@ -0,0 +1,34 @@
+#imports
+import numpy as np
+from bayes_opt import BayesianOptimization
+import matplotlib.pyplot as plt
+
+#now import the code form the other files
+import sys
+sys.path.append('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\code\\Synth\\Optimising-Field-Synthesiser\\BO\\')
+sys.path.append('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\code\\Synth\\Optimising-Field-Synthesiser\\BO\\synthesiser_simulation\\')
+from subtargetfunctions import *
+from bossfunction import *
+from field_synth_class import *
+#from ErrorCorrectionFunction import * #out of date functions
+from ErrorCorrectionFunction_integrate import *
+
+"""
+test if the bossfunction works to create a sharp peak by varying for example the CEPs
+"""
+#create fields
+Field1=Wavepacket(t0=0.0, wavel=400.0, fwhm=10.0, amp=1.0, CEP=0.0)
+Field2=Wavepacket(t0=0.0, wavel=700.0, fwhm=20.0, amp=1.0, CEP=0.0)
+Field3=Wavepacket(t0=0.0, wavel=1000.0, fwhm=30.0, amp=1.0, CEP=0.0)
+Field4=Wavepacket(t0=0.0, wavel=2000.0, fwhm=10.0, amp=1.0, CEP=0.0)
+Field5=Wavepacket(t0=0.0, wavel=1500.0, fwhm=15.0, amp=1.0, CEP=0.0)
+
+#initialise pulse list and tuple of relative delays (from 1st pulse)
+pulses=[Field1,Field2, Field3, Field4, Field5]
+delays=(10,20,30,10)
+#pass to synthesiser
+Synth=Synthesiser(pulses,delays)
+
+#parameters to be optimised
+params=['CEP5','CEP2','CEP3','CEP4']
+BO(params, Synth, sharpestPeak_triang, 10,10)
