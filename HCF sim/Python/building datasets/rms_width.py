40ec39235acff1a618638aae69476f7c3cb32bee
Molly Smith <93262796+iammollys@users.noreply.github.com>
2022-02-03 21:09:15 +0000

    Update rms_width.py
    Started trying pump-probe method to measure width - not working yet

--- HCF sim/Python/building datasets/rms_width.py
+++ HCF sim/Python/building datasets/rms_width.py
@@ -15,3 +15,28 @@ def rms_width(omega,I):
     returns the rms width of an intensity distribution over angular frequencies omega
     """
     return ((moment(omega,I,2)/moment(omega,I,0))-(moment(omega,I,1)/moment(omega,I,0))**2)**0.5
+
+def pump_probe_width(omega, I):
+    g = []
+
+    for i in range(1, len(omega)+1):
+        I_cut = I[:i]
+        I_shifted = np.append(I_cut, np.zeros(len(omega)-i))
+        g_tau = integrate.simps(I_shifted*I, omega)
+        g.append(g_tau)
+    for i in range(1, len(omega)):
+        I_cut = I[i:]
+        I_shifted = np.zeros(i)
+        I_shifted = np.append(I_shifted, I_cut)
+        g_tau = integrate.simps(I_shifted*I, omega)
+        g.append(g_tau)
+    
+    tau_axis = np.append(-omega, omega[1:])
+    
+    N_2 = integrate.simps(g, tau_axis)
+    variance_unnormalised = integrate.simps(g*(tau_axis**2), tau_axis)
+    variance = variance_unnormalised/(2*N_2)
+    print(variance)
+    
+def gauss(x, A, u, o):
+    return A*np.exp(((x-u)**2)/(o**2))
