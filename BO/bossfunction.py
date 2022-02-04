860b93794a3be20d7cc524f0bfe39d989e5faac5
MaikeLenz <maikelenz5@gmail.com>
2022-02-04 15:10:14 +0000

    tested ramp

--- BO/bossfunction.py
+++ BO/bossfunction.py
@@ -89,7 +89,8 @@ def BO(params, Synth, function, init_points=50, n_iter=50, goal_field=None, t=np
     for i in params:
         #assume standard bounds, same for each channel
         if 'wavel' in i:
-            pbounds[i] = (400,2000)
+            #pbounds[i] = (400,2000)
+            pbounds[i] = (400,10000)
         if 'fwhm' in i:
             pbounds[i] = (5,70)
         if 'amp' in i:
