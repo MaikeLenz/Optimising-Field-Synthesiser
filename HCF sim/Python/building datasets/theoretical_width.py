1cdd0d962655bc9726b8498646cd4ce7920fe9b4
iammollys <iammollysmith@gmail.com>
2022-02-07 17:00:34 +0000

    updated theoretical width

--- HCF sim/Python/building datasets/theoretical_width.py
+++ HCF sim/Python/building datasets/theoretical_width.py
@@ -14,13 +14,13 @@ def theoretical_width(flength, pressure, λ0, τfwhm, energy):
 
     # Right now this is just a rough estimate for n2, need an actual function
     if pressure <= 10*(10**(-3)):
-        n2 = 0 * (10**(-9))
+        n2 = 0 * (10**(-19))
     elif pressure > 10*(10**(-3)) and pressure <= 100*(10**(-3)):
-        n2 = 0.1 * (10**(-9))
+        n2 = 0.1 * (10**(-19))
     elif pressure > 100*(10**(-3)) and pressure <= 500*(10**(-3)):
-        n2 = 0.8 * (10**(-9))
+        n2 = 0.8 * (10**(-19))
     elif pressure > 500*(10**(-3)):
-        n2 = 1.3 * (10**(-9))
+        n2 = 1.3 * (10**(-19))
     
 
     return 2*np.sqrt(2)*np.exp(-0.5)*w0*n2*I0*L/(c*τ)
