9179fedc66c5f8522aafbf2f37e83d84ea69ee4f
MaikeLenz <maikelenz5@gmail.com>
2022-02-07 10:30:29 +0000

    started building datasets

---
+++ HCF sim/Python/building datasets/energy_and_duration_vs_rms_width.py
@@ -0,0 +1,60 @@
+import julia
+import matplotlib.pyplot as plt
+
+julia.Julia(runtime="C:\\Users\\ML\\AppData\\Local\\Programs\\Julia-1.7.0\\bin\\julia.exe")
+
+from julia import Main
+
+Main.using("Luna")
+
+from rms_width import *
+# Arguments
+radius = 125e-6 # HCF core radius
+flength = 1 # HCF length
+gas = "Ne"
+pressure = 2.340607 # gas pressure in bar, corresponds to 66% of 3.5 atm
+λ0 = 800e-9 # central wavelength of the pump pulse
+energies = np.linspace(0.1e-3,4.1e-3,2) # array of energies in the pump pulse
+τfwhms = np.linspace(5e-15,60e-15,2) # array of FWHM durations of the pump pulse
+
+# Assign arguments to Main namespace
+Main.radius = radius
+Main.flength = flength
+
+Main.gas_str = gas
+Main.eval("gas = Symbol(gas_str)")
+
+Main.pressure = pressure
+Main.λ0 = λ0
+widths=np.zeros((len(τfwhms),len(energies)))
+
+for i in energies:
+    print(i)
+    Main.energy = i
+    for j in τfwhms:
+        Main.τfwhm = j
+
+        # Calculations
+        # setting pressure to (0,pressure) means a gradient from zero up until given value
+        Main.duv = Main.eval('duv = prop_capillary(radius, flength, gas, pressure; λ0, τfwhm, energy, trange=400e-15, λlims=(150e-9, 4e-6))')
+
+        #now extract datasets
+        Main.eval("ω, Iω = Processing.getIω(duv, :ω, flength)")
+        Main.eval('t, Et = Processing.getEt(duv)')
+
+        ## These next lines show how t, Et and zactual could be accessed in the Python namespace for analysis
+        # Once defined, the same method as in time_1d can be used to get the y data needed for analysis of the curve (max heigh, FWHM, etc.), which
+        # subsequently would be the inputs the the BO 
+        #assign python variables
+        ω = Main.ω
+        Iω = Main.Iω
+        width=rms_width(ω,Iω)
+        widths[i][j]=width
+
+N = int(len(widths)**.5)
+z = widths.reshape(N, N)
+plt.imshow(z, extent=(np.amin(energies), np.amax(energies), np.amin(τfwhms), np.amax(τfwhms)), aspect = 'auto')
+plt.ylabel("Pulse duration, s")
+plt.xlabel("Pulse energy, J")
+plt.colorbar()
+plt.show()
