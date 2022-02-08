94f7428419098192ff32ed3859428319615dc050
MaikeLenz <maikelenz5@gmail.com>
2022-02-08 16:43:18 +0000

    added FT

--- BO/synthesiser_simulation/chirp/pulse_with_GDD.py
+++ BO/synthesiser_simulation/chirp/pulse_with_GDD.py
@@ -22,28 +22,22 @@ def efield_time_domain(t, amp, om0, dom, t0, gdd, cep):
     q = ( np.log(4)/dom **2 + 0.5 * 1j * gdd )**0.5
     return np.real(amp * np.log(4)**0.5 * np.exp(-0.25*(t-t0)**2/q**2) * np.exp(1j*om0*(t-t0) + 1j*cep)/(dom*q))
 
-c = 300 # nm/fs
-lam0 = 800 # nm
-tau = 4 # fs, for TL pulse
 
-amp = 1
-om0 = 2 * np.pi * c/lam0 # centre frequency
-dom = 2 * np.pi * 0.441/tau # bandwidth of TL Gaussian pulse with fwhm = tau
-t0 = 0
-cep = 0
-
-t = np.linspace(-20, 20, 1000)
-
-gdd = 0
-eft = efield_time_domain(t, amp, om0, dom, t0, gdd, cep)
-plt.plot(t, eft, label="GDD=0")
-
-
-
-gdd = 20
-eft = efield_time_domain(t, amp, om0, dom, t0, gdd, cep)
-plt.plot(t, eft, label="GDD="+str(gdd))
-plt.legend()
-plt.show()
+def efield_freq_domain(t, amp, om0, dom, t0, gdd, cep):
+    """ returns time domain e_efield (real)
+    t is time axis
+    amp is pulse amplitude
+    om0 is centre frequency
+    dom is bandwidth
+    t0 is time offset
+    gdd is group delay dispersion (in units of time^2)
+    cep is carrier envelop phase
+    """
+    
+    q = ( np.log(4)/dom **2 + 0.5 * 1j * gdd )**0.5
+    E=amp * np.log(4)**0.5 * np.exp(-0.25*(t-t0)**2/q**2) * np.exp(1j*om0*(t-t0) + 1j*cep)/(dom*q)
+    E_omega=np.fft.ifft(E)
+    omega=np.fft.fftfreq(len(t),d=(t[1]-t[0]))
+    return omega,np.real(E_omega)
 
 
---
+++ BO/synthesiser_simulation/chirp/test_GDD_FFT.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from sympy import E
+from pulse_with_GDD import *
+
+import numpy as np
+import matplotlib.pylab as plt
+
+"""
+c = 300 # nm/fs
+lam0 = 800 # nm
+tau = 4 # fs, for TL pulse
+
+amp = 1
+om0 = 2 * np.pi * c/lam0 # centre frequency
+print(om0)
+dom = 2 * np.pi * 0.441/tau # bandwidth of TL Gaussian pulse with fwhm = tau
+t0 = 0
+cep = 0
+
+t = np.linspace(-20, 20, 1000)
+
+gdd = 0
+#eft = efield_time_domain(t, amp, om0, dom, t0, gdd, cep)
+#plt.plot(t, eft, label="GDD=0")
+omega,E_omega=efield_freq_domain(t, amp, om0, dom, t0, gdd, cep)
+plt.plot(omega, E_omega, label="GDD=0")
+
+gdd = 20
+#eft = efield_time_domain(t, amp, om0, dom, t0, gdd, cep)
+omega,E_omega=efield_freq_domain(t, amp, om0, dom, t0, gdd, cep)
+plt.plot(omega, E_omega, label="GDD="+str(gdd))
+
+#plt.plot(t, eft, label="GDD="+str(gdd))
+plt.legend()
+plt.show()
+"""
+
+t=np.linspace(0,100,10000)
+f=10
+Et=np.sin(f*t)
+
+E_omega=np.fft.fft(Et)
+omega=np.fft.fftfreq(len(t),d=(t[1]-t[0]))
+
+plt.figure()
+plt.plot(t,Et)
+plt.figure()
+plt.plot(omega,E_omega)
+plt.show()
+
+
