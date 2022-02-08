03020930702857ea615380486b37dca849340364
iammollys <iammollysmith@gmail.com>
2022-02-08 23:20:08 +0000

    no changes

--- HCF sim/Python/building datasets/theoretical_width.py
+++ HCF sim/Python/building datasets/theoretical_width.py
@@ -27,16 +27,4 @@ def theoretical_width(radius, flength, pressure, λ0, τfwhm, energy):
     Main.eval('N0, n0, n2 = Tools.getN0n0n2(ω, gas; P=pressure, T=PhysData.roomtemp)')
     n2 = Main.n2
 
-    """
-    # Right now this is just a rough estimate for n2, need an actual function
-    if pressure <= 10*(10**(-3)):
-        n2 = 0 * (10**(-19))
-    elif pressure > 10*(10**(-3)) and pressure <= 100*(10**(-3)):
-        n2 = 0.1 * (10**(-19))
-    elif pressure > 100*(10**(-3)) and pressure <= 500*(10**(-3)):
-        n2 = 0.8 * (10**(-19))
-    elif pressure > 500*(10**(-3)):
-        n2 = 1.3 * (10**(-19))
-    """
-
     return 2*np.sqrt(2)*np.exp(-0.5)*ω0*n2*I0*L/(c*τ)
