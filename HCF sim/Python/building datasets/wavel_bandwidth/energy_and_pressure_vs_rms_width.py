54a5ef85230e571a1c944c090888d6237096842e
MaikeLenz <maikelenz5@gmail.com>
2022-02-09 21:43:25 +0000

    changed frequency to wavelength bandwidth

--- HCF sim/Python/building datasets/wavel_bandwidth/energy_and_pressure_vs_rms_width.py
+++ HCF sim/Python/building datasets/wavel_bandwidth/energy_and_pressure_vs_rms_width.py
@@ -37,16 +37,16 @@ for i in range(len(energies)):
         Main.duv = Main.eval('duv = prop_capillary(radius, flength, gas, pressure; λ0, τfwhm, energy, trange=400e-15, λlims=(150e-9, 4e-6))')
 
         #now extract datasets
-        Main.eval("ω, Iω = Processing.getIω(duv, :ω, flength)")
-        Main.eval('t, Et = Processing.getEt(duv)')
+        Main.eval("λ, Iλ = Processing.getIω(duv, :λ, flength)")
+        #Main.eval('t, Et = Processing.getEt(duv)')
 
         ## These next lines show how t, Et and zactual could be accessed in the Python namespace for analysis
         # Once defined, the same method as in time_1d can be used to get the y data needed for analysis of the curve (max heigh, FWHM, etc.), which
         # subsequently would be the inputs the the BO 
         #assign python variables
-        ω = Main.ω
-        Iω = Main.Iω
-        width=rms_width(ω,Iω)
+        λ = Main.λ
+        Iλ = Main.Iλ
+        width=rms_width(λ,Iλ)*10**9
         widths[i][j]=width
 
 plt.imshow(widths, extent=(np.amin(pressures)*10**15, np.amax(pressures)*10**15,np.amin(energies)*10**3, np.amax(energies)*10**3), aspect = 'auto', origin="lower")
@@ -54,5 +54,5 @@ plt.xlabel("Pressure, bar")
 plt.ylabel("Pulse energy, mJ")
 #legend
 cbar = plt.colorbar()
-cbar.ax.set_ylabel('angular frequency bandwidth', rotation=270, labelpad=15)
+cbar.ax.set_ylabel('Bandwidth, nm', rotation=270, labelpad=15)
 plt.show()
