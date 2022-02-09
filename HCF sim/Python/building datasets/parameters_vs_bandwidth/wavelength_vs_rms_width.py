ebed80bec631860603872d8ab572e3a3a765627b
MaikeLenz <maikelenz5@gmail.com>
2022-02-09 14:58:11 +0000

    added paths

--- HCF sim/Python/building datasets/parameters_vs_bandwidth/wavelength_vs_rms_width.py
+++ HCF sim/Python/building datasets/parameters_vs_bandwidth/wavelength_vs_rms_width.py
@@ -2,9 +2,11 @@ import julia
 import matplotlib.pyplot as plt
 #plt.rcParams['text.usetex'] = True
 import sys
+#sys.path.append("C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\code\\Synth\\Optimising-Field-Synthesiser\\HCF sim\\Python\\building datasets\\")
 sys.path.append("C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\HCF sim\\Python\\building datasets\\")
 from theoretical_width import theoretical_width
 
+julia.Julia(runtime="C:\\Users\\ML\\AppData\\Local\\Programs\\Julia-1.7.0\\bin\\julia.exe")
 julia.Julia(runtime="C:\\Users\\iammo\\AppData\\Local\\Programs\\Julia-1.7.1\\bin\\julia.exe")
 
 from julia import Main
