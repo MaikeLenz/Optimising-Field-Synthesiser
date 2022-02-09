537a2f15a292243d7f94929aec0975d5152f583a
iammollys <iammollysmith@gmail.com>
2022-02-09 15:17:34 +0000

    Testing pulse in freq domain

--- BO/synthesiser_simulation/chirp/pulse_with_GDD_freq_test.py
+++ BO/synthesiser_simulation/chirp/pulse_with_GDD_freq_test.py
@@ -7,13 +7,12 @@ import matplotlib.pyplot as plt
 
 c = 299792458 # m/s
 wavel = 800e-9
-domega = 2
+domega = 2e15
 omega = np.linspace(2*np.pi*c/wavel - domega/2, 2*np.pi*c/wavel + domega/2, 100)
 
 E = np.abs(E_field_freq(omega, GD=0.0, wavel=wavel, domega=domega, amp=1, CEP=0, GDD=0, TOD=0))
 
-print(E)
-
 plt.plot(omega, E)
 plt.xlabel('ang freq')
 plt.ylabel('electric field amplitude')
+plt.show()
