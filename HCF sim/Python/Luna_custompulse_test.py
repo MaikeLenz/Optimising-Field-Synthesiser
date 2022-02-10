5026095e03a05731a218d59f1e7c419d6e52e9ba
iammollys <iammollysmith@gmail.com>
2022-02-10 15:28:37 +0000

    Sent custom freq domain pulse to Luna

--- HCF sim/Python/Luna_custompulse_test.py
+++ HCF sim/Python/Luna_custompulse_test.py
@@ -11,6 +11,7 @@ julia.Julia(runtime="C:\\Users\\iammo\\AppData\\Local\\Programs\\Julia-1.7.1\\bi
 from julia import Main
 Main.using("Luna")
 
+# Define custom pulse in frequency domain
 energy = 0.5e-3 # energy in the pump pulse, 0.5mJ
 c = 299792458 # m/s
 wavel = 800e-9
@@ -23,6 +24,20 @@ Iω = np.abs(E**2)
 Main.energy = energy
 Main.ω = omega
 Main.Iω = Iω  
-Main.ϕω = ϕω
+Main.phase = ϕω
+Main.λ0 = wavel
 
-Main.eval('pulse = Interface.Pulses.DataPulse(ω, Iω, ϕω; energy, λ0=NaN, mode=:lowest, polarisation=:linear, propagator=nothing)')
+# Define experimental params
+radius = 125e-6 # HCF core radius
+flength = 1 # HCF length
+gas = "Ne"
+pressure = 2.340607 # gas pressure in bar, corresponds to 66% of 3.5 atm
+Main.radius = radius
+Main.flength = flength
+Main.gas_str = gas
+Main.eval("gas = Symbol(gas_str)")
+Main.pressure = pressure
+
+# Pass data to Luna
+Main.eval('pulse = Pulses.DataPulse(ω, Iω, phase; energy, λ0=NaN, mode=:lowest, polarisation=:linear, propagator=nothing)')
+Main.duv = Main.eval('duv = prop_capillary(radius, flength, gas, pressure; λ0, pulses=pulse, trange=400e-15, λlims=(150e-9, 4e-6))')
