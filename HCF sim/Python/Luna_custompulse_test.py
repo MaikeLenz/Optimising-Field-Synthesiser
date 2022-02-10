f84de246f267dedc6b69f143399a2f473162c0ba
iammollys <iammollysmith@gmail.com>
2022-02-10 15:31:20 +0000

    Plots outputs

--- HCF sim/Python/Luna_custompulse_test.py
+++ HCF sim/Python/Luna_custompulse_test.py
@@ -41,3 +41,46 @@ Main.pressure = pressure
 # Pass data to Luna
 Main.eval('pulse = Pulses.DataPulse(ω, Iω, phase; energy, λ0=NaN, mode=:lowest, polarisation=:linear, propagator=nothing)')
 Main.duv = Main.eval('duv = prop_capillary(radius, flength, gas, pressure; λ0, pulses=pulse, trange=400e-15, λlims=(150e-9, 4e-6))')
+
+#now extract datasets
+Main.eval("λ, Iλ = Processing.getIω(duv, :λ, flength)")
+Main.eval("ω, Iω = Processing.getIω(duv, :ω, flength)")
+Main.eval('t, Et = Processing.getEt(duv)')
+
+## These next lines show how t, Et and zactual could be accessed in the Python namespace for analysis
+# Once defined, the same method as in time_1d can be used to get the y data needed for analysis of the curve (max heigh, FWHM, etc.), which
+# subsequently would be the inputs the the BO 
+#assign python variables
+λ = Main.λ
+Iλ = Main.Iλ
+t = Main.t
+omega=Main.ω
+Iomega=Main.Iω
+Iomega=Iomega.reshape((-1,))[0:500]
+omega=omega[0:500]
+
+Et_allz=Main.Et #array of Et at all z 
+Et=Et_allz[:,-1] #last item in each element is pulse shape at the end
+Et0=Et_allz[:,0] #first item in each element is pulse shape at the start
+#note Et is complex
+
+#plotting
+plt.figure()
+plt.plot(λ,Iλ)
+plt.xlabel("Wavelength (m)")
+plt.ylabel("Spectral energy density (J/m)")
+
+plt.figure()
+plt.plot(omega,Iomega)
+#plt.plot(width_plot,bar_height, label="rms width")
+plt.legend()
+plt.xlabel("Angular frequency,/s")
+plt.ylabel("Intensity, a.u.")
+
+plt.figure()
+plt.plot(t,Et,label="z=1m")
+plt.plot(t,Et0,label="z=0")
+plt.xlabel("time,s")
+plt.ylabel("Electric field, a.u.")
+plt.legend()
+plt.show()
