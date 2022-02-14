dc05774e7236529a71a842c05bda23d989c45596
MaikeLenz <maikelenz5@gmail.com>
2022-02-14 09:53:44 +0000

    updated GDD bounds function

--- HCF sim/Python/GDD_range.py
+++ HCF sim/Python/GDD_range.py
@@ -15,6 +15,22 @@ julia.Julia(runtime="C:\\Users\\ML\\AppData\\Local\\Programs\\Julia-1.7.0\\bin\\
 from julia import Main
 Main.using("Luna")
 
-def GDD_range()
+sys.path.append("C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\code\\Synth\\Optimising-Field-Synthesiser\\HCF sim\\Python\\building datasets\\")
+#sys.path.append("C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\HCF sim\\Python\\building datasets\\")
+
+julia.Julia(runtime="C:\\Users\\ML\\AppData\\Local\\Programs\\Julia-1.7.0\\bin\\julia.exe")
+#julia.Julia(runtime="C:\\Users\\iammo\\AppData\\Local\\Programs\\Julia-1.7.1\\bin\\julia.exe")
+
+from rms_width import *
+c = 299792458 # m/s
+
+def GDD_range(max_duration, starting_value=10, energy,wavel,domega,omega):
+    GDD=starting_value
+    E, ϕω = E_field_freq(omega, GD=0.0, wavel=wavel, domega=domega, amp=1, CEP=0, GDD=0, TOD=0)
+    E_abs=np.abs(E)
+    width=rms_width(omega,E_abs)
+    if width >100e-15:
+        GDD-=1
+
 
 
