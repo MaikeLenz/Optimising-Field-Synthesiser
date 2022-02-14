0190cd2fe9003e139188389bdb956b8445b6a4f3
MaikeLenz <maikelenz5@gmail.com>
2022-02-14 09:58:49 +0000

    updated GDD bounds function

--- HCF sim/Python/GDD_range.py
+++ HCF sim/Python/GDD_range.py
@@ -24,13 +24,26 @@ julia.Julia(runtime="C:\\Users\\ML\\AppData\\Local\\Programs\\Julia-1.7.0\\bin\\
 from rms_width import *
 c = 299792458 # m/s
 
-def GDD_range(max_duration, starting_value=10, energy,wavel,domega,omega):
-    GDD=starting_value
-    E, ϕω = E_field_freq(omega, GD=0.0, wavel=wavel, domega=domega, amp=1, CEP=0, GDD=0, TOD=0)
+def GDD_range(max_duration, starting_value=10,wavel,domega,omega, CEP, TOD):
+    GDD=np.abs(starting_value)
+    E, ϕω = E_field_freq(omega, GD=0.0, wavel=wavel, domega=domega, amp=1, CEP=CEP, GDD=GDD, TOD=TOD)
     E_abs=np.abs(E)
     width=rms_width(omega,E_abs)
-    if width >100e-15:
-        GDD-=1
+    if width >max_duration:
+        while width>max_duration:
+            GDD-=1
+            E, ϕω = E_field_freq(omega, GD=0.0, wavel=wavel, domega=domega, amp=1, CEP=0, GDD=0, TOD=0)
+            E_abs=np.abs(E)
+            width=rms_width(omega,E_abs)
+        return GDD
+    elif width<max_duration:
+        while width<max_duration:
+            GDD+=1
+            E, ϕω = E_field_freq(omega, GD=0.0, wavel=wavel, domega=domega, amp=1, CEP=0, GDD=0, TOD=0)
+            E_abs=np.abs(E)
+            width=rms_width(omega,E_abs)
+
+
 
 
 
