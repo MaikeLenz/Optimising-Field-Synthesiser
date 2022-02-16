3033d6dc3ad8a78b2403ac4d63571c7510612b69
MaikeLenz <maikelenz5@gmail.com>
2022-02-16 10:43:40 +0000

    tested sphere outcomes

--- BO/tests/difficult_functions/Rastrigin/Rastrigin_iteration_process.py
+++ BO/tests/difficult_functions/Rastrigin/Rastrigin_iteration_process.py
@@ -66,7 +66,7 @@ print(optimizer.max)
 xcoords=[]
 ycoords=[]
 for i, res in enumerate(optimizer.res):
-    print("Iteration {}: \n\t{}".format(i, res))
+    #print("Iteration {}: \n\t{}".format(i, res))
     xcoords.append(res['params']['x'])
     ycoords.append(res['params']['y'])
 
