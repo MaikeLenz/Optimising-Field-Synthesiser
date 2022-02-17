bbae6cfc3dd82b4c1e39627da1d868e1d9e073f4
iammollys <iammollysmith@gmail.com>
2022-02-17 20:02:07 +0000

    ran neon power scan

--- HCF sim/Python/experiments/simulations/simulate_experiments.py
+++ HCF sim/Python/experiments/simulations/simulate_experiments.py
@@ -32,6 +32,7 @@ c = 299792458
 omega = np.linspace(2*np.pi*c/λ0s[0] - domegas[0]/2, 2*np.pi*c/λ0s[0] + domegas[0]/2, 100)
 
 #####################################################################################################################
+"""
 # Argon 0.8bar power scan
 gas = "Ar"
 pressure = 0.8
@@ -40,7 +41,7 @@ Main.eval("gas = Symbol(gas_str)")
 Main.pressure = pressure
 
 for i in range(len(energies)):
-    E, ϕω = E_field_freq(omega, GD=0.0, wavel=λ0s[i], domega=domegas[i], amp=1, CEP=0, GDD=i, TOD=0)
+    E, ϕω = E_field_freq(omega, GD=0.0, wavel=λ0s[i], domega=domegas[i], amp=1, CEP=0, GDD=0, TOD=0)
     Iω = np.abs(E**2)
     Main.ω = omega
     Main.Iω = Iω  
@@ -56,7 +57,42 @@ for i in range(len(energies)):
     Iλ = Main.Iλ
 
     header = ['Wavelength, nm', 'Intensity']
-    with open('C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\HCF sim\\Python\\experiments\\simulations\\data\\Ar_PowerScan_'+str(i), 'w', encoding='UTF8', newline='') as f:
+    with open('C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\HCF sim\\Python\\experiments\\simulations\\data\\Ar_PowerScan_'+str(powers[i]), 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+        for j in range(len(λ)):
+            writer.writerow([λ[j]*(10**9), Iλ[j][0]])
+    
+    plt.plot(λ, Iλ, label=str(i))
+plt.legend()
+plt.show()
+"""
+#####################################################################################################################
+# Neon 3bar power scan
+gas = "Ne"
+pressure = 3
+Main.gas_str = gas
+Main.eval("gas = Symbol(gas_str)")
+Main.pressure = pressure
+
+for i in range(len(energies)):
+    E, ϕω = E_field_freq(omega, GD=0.0, wavel=λ0s[i], domega=domegas[i], amp=1, CEP=0, GDD=0, TOD=0)
+    Iω = np.abs(E**2)
+    Main.ω = omega
+    Main.Iω = Iω  
+    Main.phase = ϕω
+    Main.energy = energies[i]
+    Main.λ0 = λ0s[i]
+
+    Main.eval('pulse = Pulses.DataPulse(ω, Iω, phase; energy, λ0=NaN, mode=:lowest, polarisation=:linear, propagator=nothing)')
+    Main.duv = Main.eval('duv = prop_capillary(radius, flength, gas, pressure; λ0, pulses=pulse, trange=400e-15, λlims=(150e-9, 4e-6))')
+    Main.eval("λ, Iλ = Processing.getIω(duv, :λ, flength)")
+
+    λ = Main.λ
+    Iλ = Main.Iλ
+
+    header = ['Wavelength, nm', 'Intensity']
+    with open('C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\HCF sim\\Python\\experiments\\simulations\\data\\Ne_PowerScan_'+str(powers[i]), 'w', encoding='UTF8', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(header)
         for j in range(len(λ)):
