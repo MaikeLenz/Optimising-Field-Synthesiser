db2b98253726cefbf828536b7d0b2f24bf78f93a
MaikeLenz <maikelenz5@gmail.com>
2022-02-17 11:16:03 +0000

    imported data

--- HCF sim/Python/experiments/input_spectra/plot_input_spectra.py
+++ HCF sim/Python/experiments/input_spectra/plot_input_spectra.py
@@ -15,17 +15,16 @@ with open ('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\dat
 data=lines[22:] #gets rid of all the stuff at the top
 for i in data:
     split=i.split("\t")
-    for j in enumerate(split):
-        columns[j].append(split[j])
+    for j ,value in enumerate(split):
+        columns[j].append(float(value))
 
 #print(lines[:23])
-print(columns[:10])
+print(columns[0][:10])
 
-"""
-wavel_nm=columns
-print(wavel_nm)
+wavel_nm=columns[0]
+#print(wavel_nm)
 intens1_2=columns[1]
-print(intens1_2)
+#print(intens1_2)
 intens1_1=columns[2]
 intens1_0=columns[3]
 intens0_9=columns[4]
@@ -49,5 +48,5 @@ plt.legend()
 plt.xlabel("Wavelength, nm")
 plt.ylabel("Intensity")
 plt.show()
-"""
+
 f.close()
