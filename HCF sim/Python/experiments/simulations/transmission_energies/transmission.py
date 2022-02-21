b64a450c7e3c969f03b67a82927bcc8a22c16d34
MaikeLenz <maikelenz5@gmail.com>
2022-02-21 16:17:42 +0000

    updated simulated energy output

--- HCF sim/Python/experiments/simulations/transmission_energies/transmission.py
+++ HCF sim/Python/experiments/simulations/transmission_energies/transmission.py
@@ -49,16 +49,6 @@ intensities = [intens1_2,intens1_1,intens1_0,intens0_9,intens0_8,intens0_7,inten
 
 omega=2*np.pi*c/(wavel_nm*10**-9)
 
-####################################################################################################
-#calculate input energies by integrating intensities
-from scipy import integrate
-energy_in=[]
-for i in intensities:
-    energy_in_i = integrate.simps(i, wavel_nm)
-    energy_in.append(energy_in_i)
-
-print(energy_in)
-
 #################################################################################
 #get experimental output files
 lines2=[]
@@ -90,67 +80,61 @@ outintens1_2=np.array(columns2[11])
 
 outintensities = [outintens1_2,outintens1_1,outintens1_0,outintens0_9,outintens0_8,outintens0_7,outintens0_6,outintens0_5,outintens0_4,outintens0_3]
 
-#################################################################################
-#find output energies
+#from powermeter readings
+exp_energies_out=np.array([780e-3,720e-3,650e-3,590e-3,525e-3,465e-3,390e-3,330e-3,265e-3,190e-3])
 
-exp_energy_out=[]
-for i in outintensities:
-    energy_out_i = integrate.simps(i, outwavel_nm)
-    exp_energy_out.append(energy_out_i)
-
-print(exp_energy_out)
-
-"""
+##################################################################################
+#simulate output spectra
 import julia
 julia.Julia(runtime="C:\\Users\\ML\\AppData\\Local\\Programs\\Julia-1.7.0\\bin\\julia.exe")
 #julia.Julia(runtime="C:\\Users\\iammo\\AppData\\Local\\Programs\\Julia-1.7.1\\bin\\julia.exe")
 from julia import Main
 Main.using("Luna")
 
-# Define custom pulse in frequency domain
-energy = 1.2e-3 # energy in the pump pulse, 1.2mJ
-wavel = (moment(wavel_nm,intens1_2,1)/moment(wavel_nm,intens1_2,0))*10**-9
-print(wavel)
-#print(omega)
-ϕω=np.zeros(len(intens1_2))
-Main.energy = energy
-Main.ω = omega[::-1]
-Main.Iω = intens1_2[::-1]
-Main.phase = ϕω
-Main.λ0 = wavel
 
-# Define experimental params
-radius = 175e-6 # HCF core radius
-flength = 1.05 # HCF length
-gas = "Ne"
-pressure = (0.001,3.) # gas pressure in bar, corresponds to 66% of 3.5 atm
-Main.radius = radius
-Main.flength = flength
-Main.gas_str = gas
-Main.eval("gas = Symbol(gas_str)")
-Main.pressure = pressure
+energies_in = np.array([1.2e-3,1.1e-3,1.0e-3,0.9e-3,0.8e-3,0.7e-3,0.6e-3,0.5e-3,0.4e-3,0.3e-3]) # energy in the pump pulse, 1.2mJ
 
-# Pass data to Luna
-Main.eval('pulse = Pulses.DataPulse(ω, Iω, phase; energy, λ0=NaN, mode=:lowest, polarisation=:linear, propagator=nothing)')
-Main.duv = Main.eval('duv = prop_capillary(radius, flength, gas, pressure; λ0, pulses=pulse, trange=400e-15, λlims=(150e-9, 4e-6))')
+sim_energies_out=[]
 
-#now extract datasets
-Main.eval("λ, Iλ = Processing.getIω(duv, :λ, flength)")
+for i in range(len(energies_in)):
+    wavel = (moment(wavel_nm,intensities[i],1)/moment(wavel_nm,intensities[i],0))*10**-9
 
-## These next lines show how t, Et and zactual could be accessed in the Python namespace for analysis
-# Once defined, the same method as in time_1d can be used to get the y data needed for analysis of the curve (max heigh, FWHM, etc.), which
-# subsequently would be the inputs the the BO 
-#assign python variables
-λ = Main.λ
-Iλ = Main.Iλ
-#plotting
-plt.figure()
-plt.plot(λ*10**9,Iλ,label="simulation")
-plt.xlabel("Wavelength (m)")
-plt.ylabel("Spectral energy density (J/m)")
-plt.plot(outwavel_nm,outintens1_2, label="experiment")
+    #print(omega)
+    ϕω=np.zeros(len(intens1_2))
+    Main.energy = energies_in[i]
+    Main.ω = omega[::-1]
+    Main.Iω = intensities[i][::-1]
+    Main.phase = ϕω
+    Main.λ0 = wavel
+
+    # Define experimental params
+    radius = 175e-6 # HCF core radius
+    flength = 1.05 # HCF length
+    gas = "Ne"
+    pressure = (0.001,3.) # gas pressure in bar, corresponds to 66% of 3.5 atm
+    Main.radius = radius
+    Main.flength = flength
+    Main.gas_str = gas
+    Main.eval("gas = Symbol(gas_str)")
+    Main.pressure = pressure
+
+    # Pass data to Luna
+    Main.eval('pulse = Pulses.DataPulse(ω, Iω, phase; energy, λ0=NaN, mode=:lowest, polarisation=:linear, propagator=nothing)')
+    Main.duv = Main.eval('duv = prop_capillary(radius, flength, gas, pressure; λ0, pulses=pulse, trange=400e-15, λlims=(150e-9, 4e-6))')
+
+    #########################################################################################
+    #find simulated output energies
+    #energy(grid, Eω; bandpass=nothing)
+    Main.eval("ω, Eω = Processing.getEω(duv, :ω, flength)")
+    Main.eval("energy=Processing.energy(duv, Eω")
+    energy_i=Main.energy
+    sim_energies_out.append(energy_i)
+
+plt.plot(energies_in,exp_energies_out,label="experimental")
+plt.plot(energies_in,np.array(sim_energies_out),label="simulation")
 plt.legend()
+plt.xlabel("Energy in, J")
+plt.ylabel("Energy out, J")
 plt.show()
 
-"""
 
