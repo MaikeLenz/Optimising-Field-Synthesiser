73a5c13f61d4478b6f140b9a5e0f76aceb16684a
MaikeLenz <maikelenz5@gmail.com>
2022-02-23 11:10:08 +0000

    computed I0, intensity of spectrum I(lambda)

--- HCF sim/Python/experiments/input_spectra/find_I0.py
+++ HCF sim/Python/experiments/input_spectra/find_I0.py
@@ -1,1 +1,15 @@
-#returns 
+#returns I0, intensity in W/cm^2 from I(lambda)
+import sys
+#sys.path.append('C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\HCF sim\\Python\\building_datasets\\')
+sys.path.append("C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\code\\Synth\\Optimising-Field-Synthesiser\\HCF sim\\Python\\building_datasets\\")
+from rms_width import *
+import numpy as np
+
+def find_I0(I,wavels,energy, a):
+    """
+    finds intensity I0 from intenstity spectrum I as function of wavelength, array wavels.
+    Pulse has energy "energy"
+    Assumes optimal coupling to HCF such that w0=0.64*a where w0= beam radius at waist, a= fibre radius
+    Assume transform limited at fibre start
+    """
+    return (energy*29979.2458*rms_width(wavels,I))/((np.pi**(3/2))*(0.64*a)**2*np.sqrt(2)*0.44*(moment(wavel_nm,intensities[i],1)/moment(wavel_nm,intensities[i],0))**2)
