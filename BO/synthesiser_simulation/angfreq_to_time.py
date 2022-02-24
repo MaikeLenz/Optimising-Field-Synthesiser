ee1a28fc1f8c230fd652ee50c6e45f37eefc6b0a
iammollys <iammollysmith@gmail.com>
2022-02-24 20:39:57 +0000

    Fourier transform code

--- BO/synthesiser_simulation/angfreq_to_time.py
+++ BO/synthesiser_simulation/angfreq_to_time.py
@@ -1,8 +1,10 @@
 #takes complex e(omega) as input and returns E(t)
 
+#from locale import ERA_T_FMT
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
+from scipy.fft import *
 
 def E_omega_to_E_time(E_om,om):
     """
@@ -31,4 +33,52 @@ def FourierTransform(Et,t):
     plt.plot(omf,E)
     plt.show()
 
+def t_to_f(t, Et):
+    """
+    Input of t and Et
+    Returns f, Ef
+    """
+    ts = t[1]-t[0] # Sampling interval
+    sr = 1/ts # Sampling rate
+    Ef = fft(Et)
+    N = len(Ef) # Number of points
+    n = np.arange(N)
+    T = N/sr
+    f = n/T
 
+    Ef_oneside = Ef[:N//2]
+    f_oneside = f[:N//2]
+    return f_oneside, Ef_oneside
+
+def f_to_t(f, Ef):
+    """
+    Input of f and Ef
+    Returns t, Et
+    """
+    Et = ifft(Ef)
+    N = len(Ef) # Number of points
+    #t = 1/f
+    fs = f[1]-f[0]
+    sr = 1/fs
+    N = len(Et)
+    n = np.arange(N)
+    T = N/sr
+    t = n/T
+    return t, Et
+
+t = np.linspace(0,1,100)
+freq = 2
+Et = 3*np.sin(2*np.pi*freq*t)
+plt.plot(t, Et, label='Before FFT')
+plt.legend()
+
+f, Ef = t_to_f(t, Et)
+plt.figure()
+plt.plot(f, Ef, label='After FFT')
+plt.legend()
+
+t2, Et2 = f_to_t(f, Ef)
+plt.figure()
+plt.plot(t2, Et2, label='After IFFT')
+plt.legend()
+plt.show()
