9dc2fc11a0a0c434eed0b283b9cae881fd36b04d
MaikeLenz <maikelenz5@gmail.com>
2022-02-24 11:46:19 +0000

    patched together FT

--- BO/synthesiser_simulation/angfreq_to_time.py
+++ BO/synthesiser_simulation/angfreq_to_time.py
@@ -1,6 +1,7 @@
 #takes complex e(omega) as input and returns E(t)
 
 import numpy as np
+import scipy as sp
 import matplotlib.pyplot as plt
 
 def E_omega_to_E_time(E_om,om):
@@ -11,8 +12,10 @@ def E_omega_to_E_time(E_om,om):
 
     Fs=2*(om.max()/(2*np.pi)) #samplicng frequency
     n=len(E_om)
-    t = np.arange(0, n/Fs, 1/Fs)
-    E_t = np.fft.ifft(E_om)
+    E_t = sp.fft.ifft(E_om)
+    t = np.linspace(0,len(E_t)/Fs,len(E_t))
+    #t=np.append(t[len(t)/2:],t[:len(t)/2])
+    E_t=np.append(E_t[int(len(E_t)/2):],E_t[:int(len(E_t)/2)])
     plt.plot(t,np.abs(E_t))
     plt.show()
     return E_t,t
