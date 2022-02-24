dc5da3c6fec8c9e6af8abcbd191c9465d724ec7c
MaikeLenz <maikelenz5@gmail.com>
2022-02-24 13:35:12 +0000

    updated width method

--- HCF sim/Python/building_datasets/width_methods.py
+++ HCF sim/Python/building_datasets/width_methods.py
@@ -25,12 +25,12 @@ def threshold(x,y):
     Find points where signal reaches certain level above noise and find the distance between them
     """
     thresh=0.1
-    rows, cols = np.where(y > max(y)*thresh)
+    rows = np.where(y > max(y)*thresh)
     if len(rows) > 0:
-        min_index = (rows[0], cols[0])
+        min_index = rows[0]
         max_index = min_index
     else:
-        min_index = 0
-        max_index = -1
+        min_index = rows[0]
+        max_index = rows[-1]
     return x[max_index]-x[min_index]
     
