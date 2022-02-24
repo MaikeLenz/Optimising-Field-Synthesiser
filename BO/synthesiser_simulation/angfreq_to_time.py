7da88fed4d081160ec4b45fbad1d228e326cc0ce
MaikeLenz <maikelenz5@gmail.com>
2022-02-24 12:32:13 +0000

    updated FT

--- BO/synthesiser_simulation/angfreq_to_time.py
+++ BO/synthesiser_simulation/angfreq_to_time.py
@@ -8,13 +8,12 @@ def E_omega_to_E_time(E_om,om):
     """
     Fourier transforms from ang freq to time domain 
     """
-    print(E_om[np.nonzero(E_om)[-1]])
-    Fs=2*(E_om[np.nonzero(E_om)[-1]]/(2*np.pi)) #samplicng frequency
-    n=len(E_om)
+    Fs=2*(om[np.nonzero(E_om)[-1]]) #samplicng frequency
     E_t = sp.fft.ifft(E_om)
+    print(E_t)
     t = np.linspace(0,len(E_t)/Fs,len(E_t))
     #t=np.append(t[len(t)/2:],t[:len(t)/2])
     E_t=np.append(E_t[int(len(E_t)/2):],E_t[:int(len(E_t)/2)])
-    plt.plot(t,np.abs(E_t))
+    plt.plot(t,E_t)
     plt.show()
     return E_t,t
