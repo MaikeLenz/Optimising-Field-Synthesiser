9985c100210ff8af6e4e2c98bf8869379924460e
MaikeLenz <maikelenz5@gmail.com>
2022-02-24 13:18:32 +0000

    updated FT

--- BO/synthesiser_simulation/angfreq_to_time.py
+++ BO/synthesiser_simulation/angfreq_to_time.py
@@ -21,13 +21,14 @@ def E_omega_to_E_time(E_om,om):
 def FourierTransform(Et,t):
     E=sp.fft.fft(Et)
     omf=sp.fft.fftfreq(len(t))
-
+    """
     #samp=(abs(abs(x[-1])-abs(x[0]))/len(x))/1000 
     tsamp=(abs(abs(t[-1])-abs(t[0]))/len(t))/1000
     om=omf[int(len(omf)/2+1):len(omf)]
     #repx= 2*tsamp/om
     y = abs(E[int(len(omf)/2+1):len(omf)]).tolist()
-    plt.plot(om,y)
+    """
+    plt.plot(omf,E)
     plt.show()
 
 
--- BO/tests/test_angfreq_to_time.py
+++ BO/tests/test_angfreq_to_time.py
@@ -4,13 +4,13 @@ sys.path.append("C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project
 
 from angfreq_to_time import *
 import numpy as np
-"""
+
 def E_om(om):
     return np.exp(-(om-100)**2/(10)**2)
 om=np.linspace(50,150,1000)
 E=E_om(om)
 FourierTransform(E,om)
-"""
+
 """
 om=np.array([0,50,100])
 E_om=np.array([0,1,0])
