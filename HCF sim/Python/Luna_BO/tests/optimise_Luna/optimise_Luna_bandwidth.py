9c44398ea020395ec61e371d9e921e68ea0b02d7
iammollys <iammollysmith@gmail.com>
2022-02-25 16:20:20 +0000

    optimise Luna with correct params

--- HCF sim/Python/Luna_BO/tests/optimise_Luna/optimise_Luna_bandwidth.py
+++ HCF sim/Python/Luna_BO/tests/optimise_Luna/optimise_Luna_bandwidth.py
@@ -3,6 +3,7 @@ from bayes_opt import BayesianOptimization
 import matplotlib.pyplot as plt
 import julia
 import csv
+from numpy.random import *
 #julia.Julia(runtime="C:\\Users\\ML\\AppData\\Local\\Programs\\Julia-1.7.0\\bin\\julia.exe")
 julia.Julia(runtime="C:\\Users\\iammo\\AppData\\Local\\Programs\\Julia-1.7.1\\bin\\julia.exe")
 from julia import Main
@@ -25,15 +26,24 @@ from bossfunction_Luna import *
 
 params=["energy", "pressure", "radius", "flength"]
 
-init_points = 1
+init_points = 0
 n_iter = 0
 FWHM = 30e-15
 wavel = 800e-9
 gas = 'Ne'
 GDD = 0
 
+#radius_init = 125e-6
+#flength_init = 1
+#pressure_init = 2
+#energy_init = 0.5e-3
+radius_init = randint(50, 500)*(10**-6)
+flength_init = randint(1, 30)*0.1
+pressure_init = randint(1, 10)
+energy_init= randint(1, 10)*(10**-4)
+
 #values:  radius, flength, gas, pressure, wavelength, GDD, energy
-initial_values_HCF=[125e-6, 1, gas, 2, wavel, GDD, 0.5e-3]
+initial_values_HCF=[radius_init, flength_init, gas, pressure_init, wavel, GDD, energy_init]
 
 opt_dict = Luna_BO(params, initial_values_HCF, function=max_bandwidth, init_points=init_points, n_iter=n_iter, FWHM=FWHM)
 width = opt_dict['target']
@@ -44,7 +54,7 @@ flength = opt_dict['params']['flength']
 
 # Save the data
 header = ['init_points', 'n_iter', 'width, nm', 'energy, J', 'pressure, bar', 'radius, m', 'flength, m', 'FWHM, s', 'wavel, m', 'gas']
-with open('C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\HCF sim\\Python\\Luna_BO\\tests\\optimise_Luna_data\\init_' + str(init_points) + '_niter_' + str(n_iter) + '.csv', 'w', encoding='UTF8', newline='') as f:
+with open('C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\HCF sim\\Python\\Luna_BO\\tests\\optimise_Luna_data\\random_init_' + str(init_points) + '_niter_' + str(n_iter) + '.csv', 'w', encoding='UTF8', newline='') as f:
     writer = csv.writer(f)
     # write the header
     writer.writerow(header)
