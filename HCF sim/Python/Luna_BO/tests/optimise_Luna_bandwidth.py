74050338e2fecacd6bbec7e6d1916944adf0c448
iammollys <iammollysmith@gmail.com>
2022-02-25 14:24:42 +0000

    optimise the Luna simulation

---
+++ HCF sim/Python/Luna_BO/tests/optimise_Luna_bandwidth.py
@@ -0,0 +1,31 @@
+import numpy as np
+from bayes_opt import BayesianOptimization
+import matplotlib.pyplot as plt
+import julia
+#julia.Julia(runtime="C:\\Users\\ML\\AppData\\Local\\Programs\\Julia-1.7.0\\bin\\julia.exe")
+julia.Julia(runtime="C:\\Users\\iammo\\AppData\\Local\\Programs\\Julia-1.7.1\\bin\\julia.exe")
+from julia import Main
+
+import sys
+#sys.path.append('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\code\\Synth\\Optimising-Field-Synthesiser\\BO\\')
+#sys.path.append('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\code\\Synth\\Optimising-Field-Synthesiser\\BO\\synthesiser_simulation\\')
+
+sys.path.append('C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\BO\\')
+sys.path.append('C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\BO\\synthesiser_simulation\\')
+
+#sys.path.append('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\code\\Synth\\Optimising-Field-Synthesiser\\HCF sim\\Python\\Luna_BO\\')
+sys.path.append('C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\HCF sim\\Python\\Luna_BO\\')
+from bossfunction_Luna import *
+
+#this function carries out BO for hollow core fibre
+#params to be varied: 
+    # Pulse: input energy, τfwhm, central wavelength
+    # Fibre: pressure, fibre core radius, fibre length
+
+params=["energy", "pressure", "radius", "flength"]
+
+#values:  radius, flength, gas, pressure, wavelength, GDD, energy
+initial_values_HCF=[125e-6, 3, "Ne", 2, 800e-9, 0, 1e-3]
+
+Luna_BO(params, initial_values_HCF, function=max_bandwidth, init_points=1, n_iter=1, FWHM=30e-15)
+ 
