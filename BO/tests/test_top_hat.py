8dd80602fdee65cae738679621cb49ebf82ba18b
MaikeLenz <maikelenz5@gmail.com>
2022-02-28 16:42:09 +0000

    testing top hat function

---
+++ BO/tests/test_top_hat.py
@@ -0,0 +1,58 @@
+#imports
+import numpy as np
+from bayes_opt import BayesianOptimization
+import matplotlib.pyplot as plt
+
+#now import the code form the other files
+import sys
+sys.path.append('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\code\\Synth\\Optimising-Field-Synthesiser\\BO\\')
+sys.path.append('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\code\\Synth\\Optimising-Field-Synthesiser\\BO\\synthesiser_simulation\\')
+from subtargetfunctions import *
+from bossfunction import *
+from field_synth_class import *
+#from ErrorCorrectionFunction import * #out of date functions
+from ErrorCorrectionFunction_integrate import *
+
+"""
+test if the bossfunction works to mimic rms error and reproduce a linear ramp
+"""
+#create fields
+Field1=Wavepacket(t0=50.0, wavel=400.0, fwhm=40.0, amp=1.0, CEP=0.0)
+Field2=Wavepacket(t0=50.0, wavel=700.0, fwhm=40.0, amp=1.0, CEP=0.0)
+Field3=Wavepacket(t0=50.0, wavel=1000.0, fwhm=40.0, amp=1.0, CEP=0.0)
+Field4=Wavepacket(t0=50.0, wavel=2000.0, fwhm=40.0, amp=1.0, CEP=0.0)
+Field5=Wavepacket(t0=50.0, wavel=1500.0, fwhm=40.0, amp=1.0, CEP=0.0)
+Field6=Wavepacket(t0=50.0, wavel=700.0, fwhm=40.0, amp=1.0, CEP=0.0)
+Field7=Wavepacket(t0=50.0, wavel=1000.0, fwhm=40.0, amp=1.0, CEP=0.0)
+Field8=Wavepacket(t0=50.0, wavel=2000.0, fwhm=40.0, amp=1.0, CEP=0.0)
+Field9=Wavepacket(t0=50.0, wavel=1500.0, fwhm=40.0, amp=1.0, CEP=0.0)
+Field10=Wavepacket(t0=50.0, wavel=700.0, fwhm=40.0, amp=1.0, CEP=0.0)
+Field11=Wavepacket(t0=50.0, wavel=1000.0, fwhm=40.0, amp=1.0, CEP=0.0)
+Field12=Wavepacket(t0=50.0, wavel=2000.0, fwhm=40.0, amp=1.0, CEP=0.0)
+Field13=Wavepacket(t0=50.0, wavel=1500.0, fwhm=40.0, amp=1.0, CEP=0.0)
+Field14=Wavepacket(t0=50.0, wavel=4000.0, fwhm=40.0, amp=1.0, CEP=0.0)
+Field15=Wavepacket(t0=50.0, wavel=1500.0, fwhm=40.0, amp=1.0, CEP=0.0)
+#initialise pulse list and tuple of relative delays (from 1st pulse)
+pulses=[Field1,Field2, Field3, Field4, Field5,Field6,Field7,Field8,Field9,Field10,Field11,Field12,Field13,Field14,Field15]
+delays=(0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+#pass to synthesiser
+Synth=Synthesiser(pulses,delays)
+
+#define goal field
+#note: the time array must have the same spacing for the synthesiser and for the goal field!
+t=np.linspace(0,100,10000)
+#we want 50fs, so this is fraction of the whole t array
+t_goal=t[:int((30/100)*10000)]
+print(len(t_goal))
+I_goal_mid=10*np.ones(len(t_goal)-20)
+I_goal1=np.append(np.zeros((10)),I_goal_mid)
+I_goal=np.append(I_goal1,np.zeros((10)))
+print(len(I_goal))
+#parameters to be optimised
+params=['wavel1','wavel2','wavel3','wavel4','wavel5','wavel6','wavel7','wavel8','wavel9','wavel10','wavel11','wavel12','wavel13','wavel14','wavel15',
+'CEP1','CEP2','CEP3','CEP4','CEP5','CEP6','CEP7','CEP8','CEP9','CEP10','CEP11','CEP12','CEP13','CEP14','CEP15',
+'fwhm1','fwhm2','fwhm3','fwhm4','fwhm5','fwhm6','fwhm7','fwhm8','fwhm9','fwhm10','fwhm11','fwhm12','fwhm13','fwhm14','fwhm15',
+'amp1','amp2','amp3','amp4','amp5','amp6','amp7','amp8','amp9','amp10','amp11','amp12','amp13','amp14','amp15','delay1','delay2','delay3','delay4','delay5','delay6','delay7']
+BO(params, Synth, errorCorrectionAdvanced_int, 500,500, t=t,goal_field=I_goal)
+
+#outcome
