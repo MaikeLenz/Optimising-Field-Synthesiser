0bb34d5c532a64fb8be106e41cc15ddd14d55157
MaikeLenz <maikelenz5@gmail.com>
2022-03-02 12:17:22 +0000

    updated transmission plots

--- HCF sim/Python/experiments/simulations/transmission_energies/Ne_PressureScan_transmission.py
+++ HCF sim/Python/experiments/simulations/transmission_energies/Ne_PressureScan_transmission.py
@@ -156,18 +156,21 @@ scaling = transmission_actual[-1]/transmission_sim[-1]
 transmission_sim_scaled=transmission_sim*scaling
 
 delta_lambda=rms_width(wavel_nm,intens1_1)
-I0=str(round(find_I0(intens1_1,wavel_nm*10**-9,delta_lambda*10**-9,energy_in,0.5*175e-6)*10**-13,1))+"e13"
+I0=str(round(find_I0(intens1_1,wavel_nm*10**-9,delta_lambda*10**-9,energy_in,0.5*175e-6)*10**-13,1))
 
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
-
+ax1.set_title("Neon Pressure Scan, 1.1mJ",fontsize=20)
+plt.yticks(fontsize=14)
+ax1.set_xticks(list(pressures))
+plt.xticks(fontsize=14)
 #a = np.cos(2*np.pi*np.linspace(0, 1, 60.))
-ax1.plot(np.array(pressures), transmission_actual,label="experiment, I0="+I0+"W/cm^2",marker="+",ls="None")
-ax1.plot(np.array(pressures),transmission_sim_scaled,label="simulation, scaled down to %s"%(round(scaling,2)),marker="+",ls="None")
+ax1.plot(np.array(pressures), transmission_actual,label="experiment, I0="+I0+"x"+"$\mathrm{10^{13} W/cm^2}$",marker="+",ls="None")
+ax1.plot(np.array(pressures),transmission_sim_scaled,label="simulation times %s"%(round(scaling,2)),marker="+",ls="None")
 ax1.set_xlim(0.9,3.7)
-ax1.set_xlabel("Pressure, Bar")
-ax1.set_ylabel("Transmission")
-plt.legend()
+ax1.set_xlabel("Pressure, Bar",fontsize=16)
+ax1.set_ylabel("Transmission",fontsize=16)
+plt.legend(fontsize=14)
 """
 ax2 = ax1.twiny()
 ax2.set_xlabel("Input Intensity (I0), W/cm^2")
