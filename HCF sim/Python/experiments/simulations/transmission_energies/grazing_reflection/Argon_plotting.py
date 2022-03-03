2e67dbb81d93e518ccc135064bbf35181f1c4c5d
MaikeLenz <maikelenz5@gmail.com>
2022-03-03 15:43:08 +0000

    plotted grazing reflection decay coefficient

---
+++ HCF sim/Python/experiments/simulations/transmission_energies/grazing_reflection/Argon_plotting.py
@@ -0,0 +1,107 @@
+import sys
+from matplotlib.cbook import ls_mapper
+import matplotlib.pyplot as plt
+from scipy import integrate
+import csv
+import pandas as pd
+import julia
+import numpy as np
+from scipy.optimize import curve_fit
+#sys.path.append('C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\HCF sim\\Python\\building_datasets\\')
+#sys.path.append("C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\code\\Synth\\Optimising-Field-Synthesiser\\HCF sim\\Python\\building_datasets\\")
+#from rms_width import *
+
+#sys.path.append("C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\code\\Synth\\Optimising-Field-Synthesiser\\HCF sim\\Python\\experiments\\input_spectra\\")
+#sys.path.append("C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\HCF sim\\Python\\experiments\\input_spectra\\")
+#from find_I0 import *
+
+
+# rebuild sim_transmissions
+gas="Ar"
+header = ['radius', 'Fibre Length', 'Transmission']
+lines=[]
+filepath='C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\code\\Synth\\Optimising-Field-Synthesiser\\HCF sim\\Python\\experiments\\plots\\transmission\\grazing_reflection\\low_energy_transmission_' + gas+ '_mediumradii.csv'
+
+"""
+with open('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\code\\Synth\\Optimising-Field-Synthesiser\\HCF sim\\Python\\experiments\\plots\\transmission\\grazing_reflection\\low_energy_transmission_' + gas+ '_mediumradii.csv', 'w', encoding='UTF8', newline='') as f:
+    for myline in f:
+        lines.append(myline)
+
+"""
+file=open(filepath)
+csvreader = csv.reader(file)
+header = []
+header = next(csvreader)
+print(header)
+
+for row in csvreader:
+        lines.append(row)
+
+#lines=lines[1:]#remove header
+radii=[float(lines[0][0])]
+#radii=data.iloc[0][0]
+L=[]
+sims=[[]]
+for i in range(len(lines)):
+    if float(lines[i][0])!=radii[-1]:
+        radii.append(float(lines[i][0]))
+        sims.append([float(lines[i][2])])
+    else:
+        if float(lines[i][0])==radii[0]:
+            L.append(float(lines[i][1]))
+        sims[-1].append(float(lines[i][2]))
+
+
+print(radii)
+print(L)
+print(len(sims[0]))
+########################################################################################
+#fit exponential decays
+def func(x,a,b):
+    return a*np.exp(-b*x)
+fit_params=[]
+
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+for i in range(len(sims)):
+    popt,_=curve_fit(func,L,sims[i])
+    fit_params.append(popt)
+    ax1.plot(L, sims[i],label="radius=%s$\mathrm{\mu}$m"%int(radii[i]*10**6))
+plt.legend(fontsize=14,loc="upper right")
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+ax1.set_xlim(0.2,1.3)
+ax1.set_title("Argon Low Energy Transmission",fontsize=20)
+ax1.set_xlabel("Fibre Length, m",fontsize=16)
+ax1.set_ylabel("Transmission",fontsize=16)
+print(fit_params)
+####################################################################################
+
+fig2=plt.figure()
+ax2 = fig2.add_subplot(111)
+r_3=[]
+for i in radii:
+    r_3.append(i**(-3)*10**(-18))
+decay_coeff=[]
+for i in range(len(fit_params)):
+    decay_coeff.append(fit_params[i][1])
+
+r_3=np.array(r_3)
+decay_coeff=np.array(decay_coeff)
+def line(x,a):
+    return a*x
+
+popt,_=curve_fit(line,r_3,decay_coeff)
+
+ax2.plot(r_3, decay_coeff,ls="None",marker="+")
+ax2.plot(r_3,line(r_3,*popt),label=r"$\mathrm{\alpha}$=%sr$\mathrm{^{-3}}$"%(int(popt[0])))
+plt.ylabel("Decay coefficient, $\mathrm{m^{-1}}$",fontsize=16)
+plt.xlabel("Inverse of Fibre Radius cubed, $\mathrm{\mu m^{-3}}$",fontsize=16)
+plt.title("Argon Low Energy Transmission Decay Coefficient",fontsize=20)
+plt.legend(fontsize=14)
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+plt.show()
+
+
+
