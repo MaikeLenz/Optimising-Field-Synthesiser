7952a430298449965798a42f5e77b581b55e717d
iammollys <iammollysmith@gmail.com>
2022-03-04 17:18:02 +0000

    test min max intensity condition

--- HCF sim/Python/Luna_BO/tests/test_min_max_intensity_condition.py
+++ HCF sim/Python/Luna_BO/tests/test_min_max_intensity_condition.py
@@ -1,7 +1,7 @@
 # test new min/max intensity condition
 import numpy as np
 
-max_intensity = 1
+max_intensity = 1 # Can't find this in Luna yet
 min_intensity = 0
 
 E = 1e-3 # energy, J
@@ -11,3 +11,5 @@ a = 175e-6 # fibre radius, m
 I = E/((np.pi**(3/2))*tau*((0.64*a)**2))
 
 print(I)
+print(min_intensity <= I <= max_intensity)
+print(int(min_intensity <= I <= max_intensity))
