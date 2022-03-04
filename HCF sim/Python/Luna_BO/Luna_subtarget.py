255741bab9dc365c58376829029610d5f20399f5
MaikeLenz <maikelenz5@gmail.com>
2022-03-04 10:51:01 +0000

    added peak power from E squared as subtarget

--- HCF sim/Python/Luna_BO/Luna_subtarget.py
+++ HCF sim/Python/Luna_BO/Luna_subtarget.py
@@ -6,6 +6,7 @@ import sys
 sys.path.append('C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\HCF sim\\Python\\building_datasets\\')
 from rms_width import *
 
+import numpy as np 
 
 #julia.Julia(runtime="C:\\Users\\ML\\AppData\\Local\\Programs\\Julia-1.7.0\\bin\\julia.exe")
 julia.Julia(runtime="C:\\Users\\iammo\\AppData\\Local\\Programs\\Julia-1.7.1\\bin\\julia.exe")
@@ -14,3 +15,9 @@ from julia import Main
 
 def max_bandwidth(t,Et,λ,Iλ):
     return rms_width(λ,Iλ)
+
+def max_peak_power(t,Et,λ,Iλ):
+    I=()
+    for i in Et:
+        I.append(np.abs(i**2))
+    return max(I)
