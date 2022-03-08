e2ddea018fccbd2a4801a5bc8118d5b8342873a2
MaikeLenz <maikelenz5@gmail.com>
2022-03-08 11:33:04 +0000

    worked on comparing GDD effect on width

--- HCF sim/Python/experiments/simulations/GDD_effect/effect_of_GDD_input_widths.py
+++ HCF sim/Python/experiments/simulations/GDD_effect/effect_of_GDD_input_widths.py
@@ -18,29 +18,32 @@ sys.path.append("C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project
 from theoretical_width import *
 from theoretical_GDD_duration import *
 
-
+"""
 julia.Julia(runtime="C:\\Users\\ML\\AppData\\Local\\Programs\\Julia-1.7.0\\bin\\julia.exe")
 #julia.Julia(runtime="C:\\Users\\iammo\\AppData\\Local\\Programs\\Julia-1.7.1\\bin\\julia.exe")
 
 from julia import Main
 
 Main.using("Luna")
-
+"""
 sys.path.append('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\code\\Synth\\Optimising-Field-Synthesiser\\HCF sim\\Python\\building_datasets\\')
 #sys.path.append('C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\HCF sim\\Python\\building_datasets\\')
 
 from rms_width import *
+from width_methods import *
 c = 299792458 # m/s
 n=5
 gdd_step=200e-30 #in fs^2
 gdd_mid=0
-GDDs=[gdd_mid-4*gdd_step,gdd_mid-3*gdd_step,gdd_mid-2*gdd_step,gdd_mid-gdd_step,gdd_mid,gdd_mid+gdd_step,gdd_mid+2*gdd_step,gdd_mid+3*gdd_step,gdd_mid+4*gdd_step]
+#GDDs=[gdd_mid-4*gdd_step,gdd_mid-3*gdd_step,gdd_mid-2*gdd_step,gdd_mid-gdd_step,gdd_mid,gdd_mid+gdd_step,gdd_mid+2*gdd_step,gdd_mid+3*gdd_step,gdd_mid+4*gdd_step]
+GDDs=np.linspace(-1000e-30,1000e-30,20)
+
 
 # Define fixed params
 c = 299792458 
 wavel=800e-9
 energy=0.5e-3
-
+"""
 gas = "Ne"
 Main.gas_str = gas
 Main.eval("gas = Symbol(gas_str)")
@@ -52,6 +55,7 @@ flength=1.05
 Main.flength=flength
 Main.λ0 = wavel
 Main.energy=energy
+"""
 
 def fwhm_to_sigma(fwhm, power=2):
     return fwhm / (2 * (2 * np.log(2))**(1 / power))
@@ -65,10 +69,10 @@ t0=0
 zero_GDD_shape=Gauss(t,t0,fwhm_to_sigma(fwhm_duration))
 
 Et_in=[]
-Iom_out=[]
-om_out=[]
-Et_out=[]
-t_out=[]
+#Iom_out=[]
+#om_out=[]
+#Et_out=[]
+#t_out=[]
 dom=2*np.pi*0.441/fwhm_duration
 
 omega=np.linspace(2*np.pi*c/wavel - dom/2, 2*np.pi*c/wavel + dom/2, 1000)
@@ -80,43 +84,66 @@ for i in range(len(GDDs)):
     Et = efield_time_domain(t, 1, 2*np.pi*c/wavel, dom, t0, GDDs[i], 0)
     E, ϕω = E_field_freq(omega, GD=0.0, wavel=wavel, domega=dom, amp=1, CEP=0, GDD=GDDs[i], TOD=0)
     Et_in.append(np.abs(Et))
-    Iω = np.abs(E**2)
-    Main.ω = omega
-    Main.Iω = Iω  
-    Main.phase = ϕω
+    #Iω = np.abs(E**2)
+    #Main.ω = omega
+    #Main.Iω = Iω  
+    #Main.phase = ϕω
 
-    Main.eval('pulse = Pulses.DataPulse(ω, Iω, phase; energy, λ0=NaN, mode=:lowest, polarisation=:linear, propagator=nothing)')
-    Main.duv = Main.eval('duv = prop_capillary(radius, flength, gas, pressure; λ0, pulses=pulse, trange=400e-15, λlims=(150e-9, 4e-6))')
+    #Main.eval('pulse = Pulses.DataPulse(ω, Iω, phase; energy, λ0=NaN, mode=:lowest, polarisation=:linear, propagator=nothing)')
+    #Main.duv = Main.eval('duv = prop_capillary(radius, flength, gas, pressure; λ0, pulses=pulse, trange=400e-15, λlims=(150e-9, 4e-6))')
     
     #now extract datasets
-    Main.eval("ω, Iω = Processing.getIω(duv, :ω, flength)")
-    Main.eval('t, Et = Processing.getEt(duv)')
+    #Main.eval("ω, Iω = Processing.getIω(duv, :ω, flength)")
+    #Main.eval('t, Et = Processing.getEt(duv)')
 
-    ω = Main.ω
-    Iω = Main.Iω
-    Iω=Iω.reshape((-1,))[0:500]
-    ω=ω[0:500]
-    Iom_out.append(Iω)
-    om_out.append(ω)
+    #ω = Main.ω
+    #Iω = Main.Iω
+    #Iω=Iω.reshape((-1,))[0:500]
+    #ω=ω[0:500]
+    #Iom_out.append(Iω)
+    #om_out.append(ω)
 
-    t_i=Main.t
-    Et_i=Main.Et
-    Et_i=Et_i[:,-1]
-    Et_out.append(Et_i)
-    t_out.append(t_i)
+    #t_i=Main.t
+    #Et_i=Main.Et
+    #Et_i=Et_i[:,-1]
+    #Et_out.append(Et_i)
+    #t_out.append(t_i)
    
-
+thresh_widths=[]
 rms_widths=[]
+superG_widths=[]
 #SPM_widths=[]
 GDD_widths=[]
-for i in range(len(Et_out)):
-    rms_widths.append(rms_width(t_out[i],Et_out[i]))
+from scipy.optimize import curve_fit
+for i in range(len(Et_in)):
+    rms_widths.append(rms_width(t,Et_in[i]))
+    thresh_widths.append(threshold(t,Et_in[i]))
+    popt, pcov = curve_fit(superGauss, t, Et_in[i])#, p0=[4000, 800, 20])
+    superG_widths.append(popt[2])
     #SPM_widths.append(theoretical_width(radius, flength, pressure, wavel, fwhm_duration, energy))
     GDD_widths.append(GDD_duration(GDDs[i],fwhm_duration))
 GDDs=np.array(GDDs)
 rms_widths=np.array(rms_widths)
+thresh_widths=np.array(thresh_widths)
+superG_widths=np.array(superG_widths)
 #SPM_widths=np.array(SPM_widths)
 GDD_widths=np.array(GDD_widths)
+
+fig, ax1 = plt.subplots()
+
+#ax2 = ax1.twinx()
+ax1.plot(GDDs*10**30,rms_widths*0.15,label="RMS Width times 0.5",color="tab:blue")
+ax1.plot(GDDs*10**30,thresh_widths*0.3,label="Threshold Width times 0.3",color="tab:green")
+ax1.plot(GDDs*10**30,superG_widths*10**(-13),label="Super Gaussian Width times $\mathrm{10^{-13}}$",color="tab:red")
+ax1.plot(GDDs*10**30, GDD_widths,label="Theoretical GDD Broadening",color="tab:orange")
+
+ax1.set_xlabel("GDD, fs$\mathrm{^2}$",fontsize=14)
+ax1.set_ylabel("Widths, s",fontsize=14)
+#ax2.set_ylabel("Theoretical GDD Broadening/ Threshold Width, s",fontsize=14)
+plt.legend(fontsize=16)
+plt.title("Width Comparison", fontsize=20)
+plt.show()
+"""
 plt.plot(GDDs*10**30,rms_widths,label="RMS Width")
 plt.plot(GDDs*10**30, GDD_widths,label="Theoretical GDD Broadening")
 #plt.plot(GDDs*10**30,SPM_widths,label="Theoretical SPM Broadening")
@@ -125,6 +152,7 @@ plt.ylabel("Width",fontsize=14)
 plt.legend(fontsize=16)
 plt.title("Width Comparison", fontsize=20)
 plt.show()
+"""
 
 
 
