62773d1d36cc163be01dc3bb0d44c3d080219029
MaikeLenz <maikelenz5@gmail.com>
2022-03-08 12:12:57 +0000

    updated output pulse width analysis

--- HCF sim/Python/experiments/simulations/GDD_effect/effect_of_GDD_output_widths.py
+++ HCF sim/Python/experiments/simulations/GDD_effect/effect_of_GDD_output_widths.py
@@ -111,34 +111,39 @@ for i in range(len(GDDs)):
 thresh_widths=[]
 rms_widths=[]
 superG_widths=[]
-#SPM_widths=[]
-GDD_widths=[]
+normint_widths=[]
+SPM_widths=[]
 from scipy.optimize import curve_fit
 for i in range(len(Et_out)):
-    rms_widths.append(rms_width(t_out[i],Et_out[i]))
-    thresh_widths.append(threshold(t_out[i],Et_out[i]))
-    popt, pcov = curve_fit(superGauss, t[i], Et_out[i])#, p0=[4000, 800, 20])
+    rms_widths.append(rms_width(om_out[i],I_out[i]))
+    thresh_widths.append(threshold(om_out[i],I_out[i]))
+    popt, pcov = curve_fit(superGauss, om_out[i], I_out[i])#, p0=[4000, 800, 20])
     superG_widths.append(popt[2])
-    #SPM_widths.append(theoretical_width(radius, flength, pressure, wavel, fwhm_duration, energy))
-    GDD_widths.append(GDD_duration(GDDs[i],fwhm_duration))
+    normint_widths.append(norm_and_int(om_out,I_out[i]))
+    GDD_width=GDD_duration(GDDs[i],fwhm_duration)
+    SPM_widths.append(theoretical_width(radius, flength, pressure, wavel, GDD_width, energy))
+
 GDDs=np.array(GDDs)
 rms_widths=np.array(rms_widths)
-#SPM_widths=np.array(SPM_widths)
-GDD_widths=np.array(GDD_widths)
+thresh_widths=np.array(thresh_widths)
+superG_widths=np.array(superG_widths)
+SPM_widths=np.array(SPM_widths)
+normint_widths=np.array(normint_widths)
 
 fig, ax1 = plt.subplots()
 
 #ax2 = ax1.twinx()
-ax1.plot(GDDs*10**30,rms_widths*0.01,label="RMS Width times 0.01",color="tab:blue")
-ax1.plot(GDDs*10**30,thresh_widths*0.3,label="Threshold Width times 0.3",color="tab:green")
-ax1.plot(GDDs*10**30,superG_widths*0.1,label="Super Gaussian Width times 0.01",color="tab:green")
-ax1.plot(GDDs*10**30, GDD_widths,label="Theoretical GDD Broadening",color="tab:orange")
+ax1.plot(GDDs*10**30,SPM_widths,label="Theoretical SPM Broadening",color="tab:blue")
+ax1.plot(GDDs*10**30,rms_widths,label="RMS Width",color="tab:orange")
+ax1.plot(GDDs*10**30,thresh_widths,label="Threshold Width",color="tab:green")
+ax1.plot(GDDs*10**30,superG_widths,label="Super Gaussian Width",color="tab:red")
+ax1.plot(GDDs*10**30,normint_widths,label="Normalised Integral",color="tab:purple")
 
 ax1.set_xlabel("GDD, fs$\mathrm{^2}$",fontsize=14)
-ax1.set_ylabel("Widths, s",fontsize=14)
+ax1.set_ylabel("Widths, s$\mathrm{^{-1}}$",fontsize=14)
 #ax2.set_ylabel("Theoretical GDD Broadening/ Threshold Width, s",fontsize=14)
 plt.legend(fontsize=16)
-plt.title("Width Comparison", fontsize=20)
+plt.title("Frequency Width Comparison Output Pulse", fontsize=20)
 plt.show()
 """
 plt.plot(GDDs*10**30,rms_widths,label="RMS Width")
