65ecf5eb0822e05d6b21adc9b96be4cc17572284
MaikeLenz <maikelenz5@gmail.com>
2022-03-11 11:30:45 +0000

    updated formula

--- HCF sim/Python/building_datasets/errors_I0_omegaSPM.py
+++ HCF sim/Python/building_datasets/errors_I0_omegaSPM.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def I0_error(sigma_energy,sigma_lambda_rms_width,sigma_coupling,sigma_lambda0):
-    return np.sqrt(sigma_energy**2+sigma_lambda_rms_width**2+sigma_coupling**2+sigma_lambda0**2)
+    return np.sqrt(sigma_energy**2+sigma_lambda_rms_width**2+sigma_coupling**2+8*sigma_lambda0**2)
 
 def omega_SPM_error(sigma_energy,sigma_lambda_rms_width,sigma_coupling,sigma_lambda0, sigma_omega0, sigma_n2,sigma_tau):
     sigma_I0=I0_error(sigma_energy,sigma_lambda_rms_width,sigma_coupling,sigma_lambda0)
