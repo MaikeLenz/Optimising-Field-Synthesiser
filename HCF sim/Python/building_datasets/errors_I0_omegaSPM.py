febf3efef3356027984ee8a8c393a17bd83fc1bf
MaikeLenz <maikelenz5@gmail.com>
2022-03-11 14:44:01 +0000

    updated errors

--- HCF sim/Python/building_datasets/errors_I0_omegaSPM.py
+++ HCF sim/Python/building_datasets/errors_I0_omegaSPM.py
@@ -8,4 +8,4 @@ def omega_SPM_error(sigma_energy,sigma_lambda_rms_width,sigma_coupling,sigma_lam
     return np.sqrt(sigma_I0**2+sigma_omega0**2+sigma_n2**2+sigma_tau**2)
 
 print(I0_error(0.1,0.01,0.05,0.1))
-print(omega_SPM_error(0.1,0.01,0.05,0.1,0.1,0.25/9.30,0.01))
+print(omega_SPM_error(0.1,0.01,0.1,0.1,0.1,0.1+0.25/9.30,0.51))
