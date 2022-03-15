3af7a0f9a1d37d5a1f0d08a319de1a2e92a5030a
MaikeLenz <maikelenz5@gmail.com>
2022-03-15 13:12:26 +0000

    updated wavel regime integration method

--- HCF sim/Python/Luna_BO/Luna_subtarget.py
+++ HCF sim/Python/Luna_BO/Luna_subtarget.py
@@ -2,14 +2,16 @@ import julia
 #from julia.api import Julia
 import matplotlib.pyplot as plt
 import sys
-#sys.path.append('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\code\\Synth\\Optimising-Field-Synthesiser\\HCF sim\\Python\\building_datasets\\')
-sys.path.append('C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\HCF sim\\Python\\building_datasets\\')
+sys.path.append('C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\code\\Synth\\Optimising-Field-Synthesiser\\HCF sim\\Python\\building_datasets\\')
+#sys.path.append('C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\HCF sim\\Python\\building_datasets\\')
 from rms_width import *
 
 import numpy as np 
 
+from scipy.integrate import simpson
+
 #julia.Julia(runtime="C:\\Users\\ML\\AppData\\Local\\Programs\\Julia-1.7.0\\bin\\julia.exe")
-julia.Julia(runtime="C:\\Users\\iammo\\AppData\\Local\\Programs\\Julia-1.7.1\\bin\\julia.exe")
+#julia.Julia(runtime="C:\\Users\\iammo\\AppData\\Local\\Programs\\Julia-1.7.1\\bin\\julia.exe")
 
 from julia import Main
 
@@ -27,3 +29,10 @@ def peak_power_window(t,Et,λ,Iλ):
     defined in the bossfunction
     """
     return True
+
+
+def max_intens_integral(λ,Iλ,bounds):
+    λ_truncated = λ[bounds[0] <λ < bounds[1]]
+    Iλ_truncated = Iλ[bounds[0] <λ < bounds[1]]
+    return simpson(Iλ,λ)
+
