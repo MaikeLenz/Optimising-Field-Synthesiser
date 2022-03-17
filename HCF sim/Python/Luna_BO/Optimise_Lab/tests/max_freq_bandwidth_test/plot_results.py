6ffdee3e9ccd23325c7311234789e71270722b57
iammollys <iammollysmith@gmail.com>
2022-03-17 19:43:03 +0000

    plotted max freq bandwidth optimisation

--- HCF sim/Python/Luna_BO/Optimise_Lab/tests/max_freq_bandwidth_test/plot_results.py
+++ HCF sim/Python/Luna_BO/Optimise_Lab/tests/max_freq_bandwidth_test/plot_results.py
@@ -144,3 +144,112 @@ for i in optimum_iters:
 print(results)
 print('Optimum is {} times larger than no iterations'.format(results[1]/results[0]))
 plt.show()
+
+######################################################################################################################
+# Plot best initial and optimum on the same plot
+
+filepath = 'C:\\Users\\iammo\\Documents\\'
+df_iter = pd.read_csv("C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\HCF sim\\Python\\Luna_BO\\Optimise_Lab\\tests\\max_freq_bandwidth_test\\Optimal_Params_optimums.csv")
+energy = df_iter.iloc[:,2] # J
+pressure = df_iter.iloc[:,3] # bar
+grating_pos = df_iter.iloc[:,4]
+
+#results = []
+
+Main.radius = 175e-6
+Main.flength = 1.05
+Main.gas_str = "Ne"
+Main.eval("gas = Symbol(gas_str)")
+Main.λ0 = 800e-9
+Main.τfwhm = 30e-15
+
+lines=[]
+columns=[[],[],[],[],[],[],[],[],[],[],[]]
+with open (filepath+'Optimising-Field-Synthesiser\\HCF sim\\Python\\experiments\\HCF_scans\\power in\\Input_Power_Scan.txt', 'rt') as myfile:  # Open lorem.txt for reading
+    for myline in myfile:
+        lines.append(myline)
+
+c = 299792458 # m/s
+data=lines[22:] #gets rid of all the stuff at the top
+data=data[int(len(data)/2):]
+for i in data:
+    cut=i.split("\t") #delimiter is \t
+    for j ,value in enumerate(cut):
+        columns[j].append(float(value))
+
+wavel_nm = np.array(columns[0])
+intens = np.array(columns[3])
+omega_list=2*np.pi*c/(wavel_nm*10**-9)
+Main.ω = omega_list[::-1]
+Main.Iω = intens[::-1]
+
+grating_pair_displacement_in = grating_pos[0]
+energy_in = energy[0]
+Main.energy = energy_in
+pressure_in = pressure[0]
+Main.pressure = pressure_in
+
+GDD, TOD = compressor_grating_values(grating_pair_displacement_mm=grating_pair_displacement_in*1000)
+phase = []
+for j in range(len(omega_list)):
+    phase.append(get_phi(omega=omega_list[j], omega0=2*np.pi*c/Main.λ0, CEP=0, GD=0, GDD=GDD, TOD=TOD, FoOD=0, FiOD=0))
+Main.phase = phase
+
+Main.eval('pulse = Pulses.DataPulse(ω, Iω, phase; energy, λ0=NaN, mode=:lowest, polarisation=:linear, propagator=nothing)')
+Main.duv = Main.eval('duv = prop_capillary(radius, flength, gas, pressure; λ0, pulses=pulse, trange=400e-15, λlims=(150e-9, 4e-6))')
+
+Main.eval("λ, Iλ = Processing.getIω(duv, :λ, flength)")
+Main.eval('t, Et = Processing.getEt(duv)')
+λ = Main.λ
+Iλ = Main.Iλ
+t = Main.t
+Et_allz = Main.Et # array of Et at all z 
+Et = Et_allz[:,-1] # last item in each element is pulse shape at the end
+f = c/λ
+
+#results.append(rms_width(λ, Iλ))
+
+plt.figure()
+plt.plot(f,Iλ, label='Best After Random Search of 50 Points')
+plt.xlabel("Frequency (/s)")
+plt.ylabel("Intensity (a.u.)")
+plt.title('Maximising Bandwidth', fontsize=24)
+
+wavel_nm = np.array(columns[0])
+intens = np.array(columns[3])
+omega_list=2*np.pi*c/(wavel_nm*10**-9)
+Main.ω = omega_list[::-1]
+Main.Iω = intens[::-1]
+
+grating_pair_displacement_in = grating_pos[299]
+energy_in = energy[299]
+Main.energy = energy_in
+pressure_in = pressure[299]
+Main.pressure = pressure_in
+
+GDD, TOD = compressor_grating_values(grating_pair_displacement_mm=grating_pair_displacement_in*1000)
+phase = []
+for j in range(len(omega_list)):
+    phase.append(get_phi(omega=omega_list[j], omega0=2*np.pi*c/Main.λ0, CEP=0, GD=0, GDD=GDD, TOD=TOD, FoOD=0, FiOD=0))
+Main.phase = phase
+
+Main.eval('pulse = Pulses.DataPulse(ω, Iω, phase; energy, λ0=NaN, mode=:lowest, polarisation=:linear, propagator=nothing)')
+Main.duv = Main.eval('duv = prop_capillary(radius, flength, gas, pressure; λ0, pulses=pulse, trange=400e-15, λlims=(150e-9, 4e-6))')
+
+Main.eval("λ, Iλ = Processing.getIω(duv, :λ, flength)")
+Main.eval('t, Et = Processing.getEt(duv)')
+λ = Main.λ
+Iλ = Main.Iλ
+t = Main.t
+Et_allz = Main.Et # array of Et at all z 
+Et = Et_allz[:,-1] # last item in each element is pulse shape at the end
+f = c/λ
+
+#results.append(rms_width(λ, Iλ))
+
+plt.plot(f,Iλ, label='Optimum Found')
+
+print(results)
+print('Optimum is {} times larger than no iterations'.format(results[1]/results[0]))
+plt.legend(fontsize=16)
+plt.show()
