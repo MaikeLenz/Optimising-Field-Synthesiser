e4619d3f9c6e0afe6136b5e5c868aa85b26c6c7c
MaikeLenz <maikelenz5@gmail.com>
2022-03-22 11:36:09 +0000

    tried optimising lab

--- HCF sim/Python/Luna_BO/bossfunction_Luna_debugging.py
+++ HCF sim/Python/Luna_BO/bossfunction_Luna_debugging.py
@@ -186,7 +186,7 @@ def Luna_BO_debug(params, initial_values_HCF, function, Gaussian = False, Imperi
         Iλ=Iλ.reshape(len(Iλ),)
         print(Iλ.shape)    
         if function==max_intens_integral:
-            return function(λ, Iλ,wavel_bounds)*power_condition
+            return function(λ, Iλ, wavel_bounds)*power_condition
         else:
             return function(t, Et, λ, Iλ)*power_condition #pass t and E to sub-target function
         
