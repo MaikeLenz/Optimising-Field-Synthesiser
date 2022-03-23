6cd68bf2456f29c1c558c4036ec5ecebe0df2f25
MaikeLenz <maikelenz5@gmail.com>
2022-03-23 16:46:48 +0000

    plotted optimised pressure distributions

--- HCF sim/Python/Luna_BO/tests/optimise_Luna/data/pressure_distributions/plot.py
+++ HCF sim/Python/Luna_BO/tests/optimise_Luna/data/pressure_distributions/plot.py
@@ -32,7 +32,7 @@ filepath="C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\code\
 #filepath="C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\HCF sim\\Python\\Luna_BO\\tests\\optimise_Luna\\data\\optimise_lab\\"
 
 # Read optimal params
-df_0 = pd.read_csv(filepath+"Ne3pressure_points__init_50_niter_150.csv")
+df_0 = pd.read_csv(filepath+"Ar3pressure_points__init_50_niter_150.csv")
 
 energy=df_0.iloc[0][3]
 radius=df_0.iloc[0][5]
