9f9b3ee4a0c235bca5b27cc1000dbf63bb190f24
iammollys <iammollysmith@gmail.com>
2022-04-01 12:27:47 +0100

    tested phase with initial GDD

--- HCF sim/Python/tests/investigate_phase/phase_test.py
+++ HCF sim/Python/tests/investigate_phase/phase_test.py
@@ -149,6 +149,7 @@ ax2.legend(loc='upper right')
 plt.suptitle('Ar, 0.4mJ, 0.66*0.2bar')
 plt.show()
 """
+"""
 # Try using Luna Gaussian pulse instead
 c = 299792458 # m/s
 wavel = 800e-9
@@ -192,4 +193,58 @@ ax2.plot(omega1[:600], (np.abs(Eomega1[:600])**2)*(10**17), label='Intensity aft
 ax1.legend(loc='upper left')
 ax2.legend(loc='upper right')
 plt.suptitle('Ne, 1.2mJ, 0.66*3bar')
+plt.show()
+"""
+# Try an inital pulse with GDD
+c = 299792458 # m/s
+wavel = 800e-9
+fwhm = 30e-15
+domega = 2*np.pi*0.441/fwhm
+radius = 175e-6 # HCF core radius
+flength = 1.05 # HCF length
+gas = "Ne"
+Main.radius = radius
+Main.flength = flength
+Main.gas_str = gas
+Main.eval("gas = Symbol(gas_str)")
+Main.λ0 = wavel
+
+energy = 1.2e-3
+pressure = 0.66*3
+Main.pressure = pressure
+Main.energy = energy
+
+omega = np.linspace(2*np.pi*c/wavel - 50*domega/2, 2*np.pi*c/wavel + 50*domega/2, 100)
+E, ϕω = E_field_freq(omega, GD=0.0, wavel=wavel, domega=domega, amp=1, CEP=0, GDD=500e-30, TOD=0)
+Iω = np.abs(E)**2
+Main.ω = omega
+Main.Iω = Iω  
+Main.phase = ϕω
+
+fig, ax1 = plt.subplots()
+ax2 = ax1.twinx()
+ax1.plot(omega, ϕω, '--', label='Phase before')
+ax2.plot(omega, Iω, label='Intensity before')
+ax1.set_xlabel('Angular Frequency')
+ax1.set_ylabel('Phase')
+ax2.set_ylabel('Intensity')
+
+# Pass data to Luna
+Main.eval('pulse = Pulses.DataPulse(ω, Iω, phase; energy, λ0=NaN, mode=:lowest, polarisation=:linear, propagator=nothing)')
+Main.duv = Main.eval('duv = prop_capillary(radius, flength, gas, pressure; λ0, pulses=pulse, trange=400e-15, λlims=(150e-9, 4e-6))')
+
+Main.eval("ω, Eω = Processing.getEω(duv)")
+
+omega1=Main.ω
+Eomega1=Main.Eω
+Eomega1=Eomega1[:,-1]
+#phase1 = np.angle(Eomega1)
+#phase1 = np.unwrap(phase1)
+phase1 = detrend(np.unwrap(np.angle(Eomega1)))
+
+ax1.plot(omega1[:600], phase1[:600], '--', label='Phase after')
+ax2.plot(omega1[:600], (np.abs(Eomega1[:600])**2)*(10**17), label='Intensity after')
+ax1.legend(loc='upper left')
+ax2.legend(loc='upper right')
+plt.suptitle('Ne, 1.2mJ, 0.66*3bar, 500fs^2 GDD')
 plt.show()
