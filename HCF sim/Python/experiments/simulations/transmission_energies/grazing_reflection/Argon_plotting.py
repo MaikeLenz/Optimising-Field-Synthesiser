5a2f19cd59ea674372d6fd0d76da3e146952c6f1
MaikeLenz <maikelenz5@gmail.com>
2022-04-14 23:22:40 +0100

    updated plotss

--- HCF sim/Python/experiments/simulations/transmission_energies/grazing_reflection/Argon_plotting.py
+++ HCF sim/Python/experiments/simulations/transmission_energies/grazing_reflection/Argon_plotting.py
@@ -7,6 +7,8 @@ import pandas as pd
 import julia
 import numpy as np
 from scipy.optimize import curve_fit
+import matplotlib.transforms as mtransforms
+
 
 #plt.rcParams['axes.prop_cycle'] = plt.cycler(color=plt.cm.Set2.colors)
 plt.style.use('tableau-colorblind10')
@@ -63,14 +65,17 @@ def func(x,a,b):
     return a*np.exp(-b*x)
 fit_params=[]
 
-fig = plt.figure()
-plt.suptitle("Argon Low Energy Transmission",fontsize=20)
+#fig = plt.figure()
+#plt.suptitle("Argon Low Energy Transmission",fontsize=20)
+fig, axs = plt.subplot_mosaic([['a)', 'b)']],constrained_layout=True)
+#ax1 = fig.add_subplot(121)
 
-ax1 = fig.add_subplot(121)
+ax1=axs["a)"]
+ax2=axs["b)"]
 for i in range(len(sims)):
     popt,_=curve_fit(func,L,sims[i])
     fit_params.append(popt)
-    ax1.plot(L, sims[i],label="radius=%s$\mathrm{\mu}$m"%int(radii[i]*10**6))
+    ax1.plot(L, sims[i],label="%s$\mathrm{\mu}$m"%int(radii[i]*10**6))
 plt.legend(fontsize=14,loc="lower right")
 plt.xticks(fontsize=14)
 plt.yticks(fontsize=14)
@@ -82,7 +87,8 @@ print(fit_params)
 ####################################################################################
 
 #fig2=plt.figure()
-ax2 = fig.add_subplot(122)
+#ax2 = fig.add_subplot(122)
+
 r_3=[]
 for i in radii:
     r_3.append(i**(-3)*10**(-18))
@@ -97,19 +103,27 @@ def line(x,a):
 
 popt,_=curve_fit(line,r_3,decay_coeff)
 
-ax2.plot(r_3, decay_coeff,ls="None",marker="+", markersize=20)
+ax2.plot(r_3, decay_coeff,ls="None",marker="+", markersize=10)
 ax2.plot(r_3,line(r_3,*popt),label=r"$\mathrm{\alpha}$=%s$\mathrm{\pm}$23$\mathrm{\mu m^{-3}}$"%(int(popt[0])))
-plt.ylabel("Decay Coefficient, $\mathrm{m^{-1}}$",fontsize=16)
-plt.xlabel("Inverse of Fibre Radius Cubed, $\mathrm{\mu m^{-3}}$",fontsize=16)
+ax2.set_ylabel("Decay Coefficient, $\mathrm{m^{-1}}$",fontsize=16)
+ax2.set_xlabel("Inverse of Core Radius Cubed, $\mathrm{\mu m^{-3}}$",fontsize=16)
 #plt.title("Argon Low Energy Transmission Decay Coefficient",fontsize=20)
 print(np.sqrt(_[0][0]))
-plt.legend(fontsize=14)
+ax1.legend(fontsize=14,loc="lower right")
+ax2.legend(fontsize=14,loc="lower right")
+
 plt.xticks(fontsize=14)
 plt.yticks(fontsize=14)
 
+"""
+for label, ax in axs.items():
+    # label physical distance in and down:
+    trans = mtransforms.ScaledTranslation(10/72, -5/72, fig.dpi_scale_trans)
+    ax.text(0.0, 1.0, label, transform=ax.transAxes + trans,
+            fontsize='large', verticalalignment='top', fontfamily='serif',
+            bbox=dict(facecolor='0.7', edgecolor='none', pad=3.0))
 
-
-
+"""
 plt.show()
 
 
