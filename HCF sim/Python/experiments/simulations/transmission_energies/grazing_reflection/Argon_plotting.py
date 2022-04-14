7dca8d8488ccf2fd1c3faaad359102a0e3ad0576
iammollys <iammollysmith@gmail.com>
2022-04-14 17:09:44 +0100

    replotted

--- HCF sim/Python/experiments/simulations/transmission_energies/grazing_reflection/Argon_plotting.py
+++ HCF sim/Python/experiments/simulations/transmission_energies/grazing_reflection/Argon_plotting.py
@@ -27,8 +27,8 @@ plt.rcParams['axes.labelsize'] = 16
 gas="Ar"
 header = ['radius', 'Fibre Length', 'Transmission']
 lines=[]
-filepath='C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\code\\Synth\\Optimising-Field-Synthesiser\\HCF sim\\Python\\experiments\\plots\\transmission\\grazing_reflection\\low_energy_transmission_' + gas+ '_mediumradii.csv'
-#filepath = 'C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\HCF sim\\Python\\experiments\\plots\\transmission\\grazing_reflection\\low_energy_transmission_' + gas+ '_mediumradii.csv'
+#filepath='C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\code\\Synth\\Optimising-Field-Synthesiser\\HCF sim\\Python\\experiments\\plots\\transmission\\grazing_reflection\\low_energy_transmission_' + gas+ '_mediumradii.csv'
+filepath = 'C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\HCF sim\\Python\\experiments\\plots\\transmission\\grazing_reflection\\low_energy_transmission_' + gas+ '_mediumradii.csv'
 
 file=open(filepath)
 csvreader = csv.reader(file)
@@ -64,20 +64,20 @@ def func(x,a,b):
 fit_params=[]
 
 fig = plt.figure()
-plt.suptitle("Argon Low Energy Transmission",fontsize=20)
+#plt.suptitle("Argon Low Energy Transmission",fontsize=20)
 
 ax1 = fig.add_subplot(121)
 for i in range(len(sims)):
     popt,_=curve_fit(func,L,sims[i])
     fit_params.append(popt)
-    ax1.plot(L, sims[i],label="radius=%s$\mathrm{\mu}$m"%int(radii[i]*10**6))
+    ax1.plot(L, sims[i],label="%s$\mathrm{\mu}$m radius"%int(radii[i]*10**6))
 plt.legend(fontsize=14,loc="lower right")
 plt.xticks(fontsize=14)
 plt.yticks(fontsize=14)
 ax1.set_xlim(0,3)
 #ax1.set_title("Argon Low Energy Transmission",fontsize=20)
-ax1.set_xlabel("Fibre Length, m",fontsize=16)
-ax1.set_ylabel("Transmission",fontsize=16)
+ax1.set_xlabel("Fibre Length (m)",fontsize=16)
+ax1.set_ylabel("Transmission Fraction",fontsize=16)
 print(fit_params)
 ####################################################################################
 
@@ -99,8 +99,8 @@ popt,_=curve_fit(line,r_3,decay_coeff)
 
 ax2.plot(r_3, decay_coeff,ls="None",marker="+", markersize=20)
 ax2.plot(r_3,line(r_3,*popt),label=r"$\mathrm{\alpha}$=%s$\mathrm{\pm}$23$\mathrm{\mu m^{-3}}$"%(int(popt[0])))
-plt.ylabel("Decay Coefficient, $\mathrm{m^{-1}}$",fontsize=16)
-plt.xlabel("Inverse of Fibre Radius Cubed, $\mathrm{\mu m^{-3}}$",fontsize=16)
+plt.ylabel("Decay Coefficient ($\mathrm{m^{-1}}$)",fontsize=16)
+plt.xlabel("Inverse of Fibre Radius Cubed ($\mathrm{\mu m^{-3}}$)",fontsize=16)
 #plt.title("Argon Low Energy Transmission Decay Coefficient",fontsize=20)
 print(np.sqrt(_[0][0]))
 plt.legend(fontsize=14)
