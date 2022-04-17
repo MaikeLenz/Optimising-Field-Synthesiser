dae719b490ec1af27745331101e22c44f2360062
iammollys <iammollysmith@gmail.com>
2022-04-17 11:41:44 +0100

    tried forcing optimisation to probe a chosen point

--- HCF sim/Python/Luna_BO/tests/optimise_with_FT_and_phase/optimise_1300nm_peak_power_time_FT_Mollypaths .py
+++ HCF sim/Python/Luna_BO/tests/optimise_with_FT_and_phase/optimise_1300nm_peak_power_time_FT_Mollypaths .py
@@ -9,6 +9,7 @@ params=["energy", "pressure", "grating_pair_displacement", "radius", "flength"]
 #values:  radius, flength, gas, pressure, wavelength, energy, τfwhm, grating_pair_separation
 gas="He"
 # Define experimental params
+"""
 radius = 100e-6 # HCF core radius
 flength = 3 # HCF length
 wavel=800e-9
@@ -16,6 +17,14 @@ duration=30e-15
 energy_init=0.75e-3
 pressure_init=0.66*3
 grating_init=0
+"""
+grating_init = 0
+energy_init = 1.5e-3
+pressure_init = 5
+radius = 150e-6
+flength = 8
+wavel=800e-9
+duration=30e-15
 initial_values_HCF=[radius, flength, gas,pressure_init, wavel, energy_init,duration, grating_init]
 
 
@@ -32,7 +41,7 @@ flength=result["params"]["flength"]
 
 header = ['init_points', 'n_iter', 'peak power', 'energy, J', 'pressure, bar', 'radius, m', 'flength, m', 'FWHM, s', 'wavel, m', 'gas', 'grating_pair_displacement, m']
 #with open('C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\HCF sim\\Python\\Luna_BO\\tests\\optimise_Luna\\data\\peak_power_1200e-9wavelwindow_varyfwhm__init_' + str(init_points) + '_niter_' + str(n_iter) + '.csv', 'a', encoding='UTF8', newline='') as f:
-with open('C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\HCF sim\\Python\\Luna_BO\\tests\\optimise_with_FT_and_phase\\data\\1300nm_phasecondition\\1300nm_He__init_50_niter_1000.csv', 'w', encoding='UTF8', newline='') as f:
+with open('C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\HCF sim\\Python\\Luna_BO\\tests\\optimise_with_FT_and_phase\\data\\1300nm_phasecondition\\1300nm_He__init_50_niter_1000_forceprobe.csv', 'w', encoding='UTF8', newline='') as f:
     writer = csv.writer(f) # peak_power_1000e-9wavelwindow__init_50_niter_100
     # write the header
     writer.writerow(header)
