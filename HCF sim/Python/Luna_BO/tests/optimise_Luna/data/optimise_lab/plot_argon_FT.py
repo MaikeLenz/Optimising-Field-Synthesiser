ed2a578c1772a9b2c8a0523f826a212c73a61b89
MaikeLenz <maikelenz5@gmail.com>
2022-04-17 22:17:15 +0100

    optimised Ar with FT

--- HCF sim/Python/Luna_BO/tests/optimise_Luna/data/optimise_lab/plot_argon_FT.py
+++ HCF sim/Python/Luna_BO/tests/optimise_Luna/data/optimise_lab/plot_argon_FT.py
@@ -23,12 +23,16 @@ from pulse_with_GDD import *
 from Luna_subtarget import *
 from compressor_grating_to_values import *
 
+sys.path.append("C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\code\\Synth\\Optimising-Field-Synthesiser\\HCF sim\\Python\\tests\\investigate_phase\\")
+#sys.path.append('C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\HCF sim\\Python\\tests\\investigate_phase\\')
+from get_phase import *
+
 Main.using("Luna")
 filepath="C:\\Users\\ML\\OneDrive - Imperial College London\\MSci_Project\\code\\Synth\\Optimising-Field-Synthesiser\\HCF sim\\Python\\Luna_BO\\tests\\optimise_Luna\\data\\optimise_lab\\"
 #filepath="C:\\Users\\iammo\\Documents\\Optimising-Field-Synthesiser\\HCF sim\\Python\\Luna_BO\\tests\\optimise_Luna\\data\\optimise_lab\\"
 
 # Read optimal params
-df_0 = pd.read_csv(filepath+"\\new_data\\Ar.csv")
+df_0 = pd.read_csv(filepath+"\\new_data\\Ar_peakpower_FT_optimise_lab__init_50_niter_1000.csv")
 
 energy=float(df_0.iloc[0][3])
 pressure=float(df_0.iloc[0][4])
@@ -78,17 +82,63 @@ Main.eval('Eω = duv["Eω"][:,end]')
 omega=Main.ω
 Eomega=Main.Eω
 
-domega = omega[2] - omega[1]
-tau = np.pi/domega
-phase_raw = np.angle(Eomega)
-phase = np.unwrap(phase_raw - omega*tau)
-phase -= phase[np.argmin(np.abs(omega - 2.4e-15))]
-phase=phase*(-1)
-omega=omega[210:340]
-phase=phase[210:340]
-Eomega=Eomega[210:340]
+phase=get_phase(omega,Eomega)
+#omega=omega[210:340]
+#phase=phase[210:340]
+#Eomega=Eomega[210:340]
+
+Et = np.fft.ifft(Eomega)
+dom = omega[2] - omega[1]
+df = dom/(2*np.pi)
+t = np.fft.fftshift(np.fft.fftfreq(len(Et), d=df))
+########################################################################   
+Main.radius = radius
+Main.flength = flength
+
+Main.gas_str = gas
+Main.eval("gas = Symbol(gas_str)")
+
+Main.pressure = 0.66*1.5
+Main.λ0 = wavel
+Main.τfwhm = FWHM
+Main.energy = 1.2e-3
+
+print(pressure,energy,grating_pair_displacement)
+domega = 2*np.pi*0.44/FWHM
+c=299792458
+om = np.linspace(2*np.pi*c/wavel -5* domega/2, 2*np.pi*c/wavel + 5*domega/2, 1000)
+
+GDD, TOD = 0,0
+
+E, ϕω = E_field_freq(om, GD=0.0, wavel=wavel, domega=domega, amp=1, CEP=0, GDD=GDD, TOD=TOD)
+Iω = np.abs(E)**2
 
 
+Main.ω = om
+Main.Iω = Iω  
+Main.phase = ϕω 
+# Pass data to Luna
+Main.eval('pulse = Pulses.DataPulse(ω, Iω, phase; energy, λ0=NaN, mode=:lowest, polarisation=:linear, propagator=nothing)')
+Main.duv = Main.eval('duv = prop_capillary(radius, flength, gas, pressure; λ0, pulses=pulse, trange=400e-15, λlims=(150e-9, 4e-6))')
+Main.eval("grid = Processing.makegrid(duv)")
+Main.eval("ω = grid.ω")
+Main.eval('Eω = duv["Eω"][:,end]')
+
+
+omega2=Main.ω
+Eomega2=Main.Eω
+
+phase2=get_phase(omega2,Eomega2)
+#omega2=omega2[210:340]
+#phase2=phase2[210:340]
+#Eomega2=Eomega2[210:340]
+
+Et2 = np.fft.ifft(Eomega2)
+dom2 = omega2[2] - omega2[1]
+df2 = dom2/(2*np.pi)
+t2 = np.fft.fftshift(np.fft.fftfreq(len(Et2), d=df2))
+    
+############################################################
 fig, ax1 = plt.subplots()
 ax2 = ax1.twinx()
 ax1.set_xlabel('Angular Frequency')
@@ -96,10 +146,14 @@ ax1.set_ylabel('Phase')
 ax2.set_ylabel('Intensity')
 ax1.plot(omega, phase, '--', label='Phase after')
 ax2.plot(omega, np.abs(Eomega)**2, label='Intensity after')
+ax1.plot(omega2, phase2, '--', label='SPM, Phase after')
+ax2.plot(omega2, np.abs(Eomega2)**2, label='SPM, Intensity after')
+
 ax1.legend(loc='upper left')
 ax2.legend(loc='upper right')
 
-plt.show()
-t,Et=f_to_t_irfft(omega/(2*np.pi),Eomega)
-plt.plot(t,np.abs(Et)**2)
+plt.figure()
+plt.plot(t,np.abs(Et)**2, label="Optimised")
+plt.plot(t2,np.abs(Et2)**2, label="SPM Optimum")
+
 plt.show()
