f2109bb066d9a04ba97467d824308244495cb55c
MaikeLenz <maikelenz5@gmail.com>
2022-04-24 13:11:25 +0100

    updated theoretical grazing incidence loss

--- HCF sim/Python/experiments/simulations/transmission_energies/grazing_reflection/theoretical_transmission_loss.py
+++ HCF sim/Python/experiments/simulations/transmission_energies/grazing_reflection/theoretical_transmission_loss.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def theoretical_alpha_um3(lam,gas):
     """
-    returns the theoretical decay coefficient from grazing incidence losses in units um^2
+    returns the theoretical decay coefficient from grazing incidence losses in units um^3 /metre
     to get the decay coefficient, need to divide this number by the core radius in um cubed
     formula from https://aip.scitation.org/doi/pdf/10.1063/1.116609
     """
@@ -11,7 +11,7 @@ def theoretical_alpha_um3(lam,gas):
     if gas=="Ne":
         n_gas=1.000066102
     nu=1.4585/n_gas
-    return 2*((2.405/(2*np.pi))**2*((lam**2)/(2))*((nu**2+1)/(np.sqrt(nu**2-1))))*10**18 #in um^2
+    return 2*((2.405/(2*np.pi))**2*((lam**2)/(2))*((nu**2+1)/(np.sqrt(nu**2-1))))*10**18 #in um^3 /m
 
 print(theoretical_alpha_um3(800e-9,"Ar"))
 print(theoretical_alpha_um3(800e-9,"Ne"))
